f080a549ba17c4d329f41cb844f6a55695efd2a7
Ruud Wiegers <ruud.wiegers@topicus.nl>
2018-12-01 08:43:51 +0100

    AoC 2018, day 1.

--- src/adventofcode/y2018/Day01.kt
+++ src/adventofcode/y2018/Day01.kt
@@ -2,14 +2,39 @@ package adventofcode.y2018
 
 import adventofcode.AdventSolution
 
-object Day01 : AdventSolution(2018, 1, "???") {
+object Day01 : AdventSolution(2018, 1, "Chronal Calibration") {
 
-	override fun solvePartOne(input: String): String {
-		return "TODO"
-	}
+	override fun solvePartOne(input: String) =
+			input.splitToSequence("\n")
+					.map(String::toInt)
+					.sum()
+					.toString()
 
 	override fun solvePartTwo(input: String): String {
-		return "TODO"
-	}
+		val deltas = input.splitToSequence("\n")
+				.map(String::toInt)
 
+		return generateSequence { deltas }
+				.flatten()
+				.scan(0, Int::plus)
+				.findFirstRepetition()
+				.toString()
+	}
+}
+
+private fun <T, R> Sequence<T>.scan(initial: R, operation: (R, T) -> R): Sequence<R> {
+	var result: R = initial
+	return this.map {
+		result = operation(result, it)
+		result
+	}
+}
+
+private fun <T> Sequence<T>.findFirstRepetition(): T? {
+	val seen = mutableSetOf<T>()
+	forEach {
+		if (it in seen) return it
+		seen += it
+	}
+	return null
 }
