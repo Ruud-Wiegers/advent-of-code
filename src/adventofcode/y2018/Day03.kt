044aac242eaa13e92f8fdc8fa5da035caabaa666
Ruud Wiegers <ruud.wiegers@topicus.nl>
2018-12-03 09:38:02 +0100

    Day 3 tidying up.

--- src/adventofcode/y2018/Day03.kt
+++ src/adventofcode/y2018/Day03.kt
@@ -4,52 +4,45 @@ import adventofcode.AdventSolution
 
 object Day03 : AdventSolution(2018, 3, "No Matter How You Slice It") {
 
-	override fun solvePartOne(input: String): String {
+    override fun solvePartOne(input: String): String {
+        val claims = parseInput(input)
+        val fabric = applyClaimsToFabric(claims)
+        return fabric.count { it > 1 }.toString()
+    }
 
-		val rule = "#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)".toRegex()
-		val claims = input.splitToSequence("\n")
-				.map { rule.matchEntire(it)!!.destructured }
-				.map { (id, x, y, w, h) -> Fabric(id, x.toInt(), y.toInt(), w.toInt(), h.toInt()) }
+    override fun solvePartTwo(input: String): String {
+        val claims = parseInput(input)
+        val fabric = applyClaimsToFabric(claims)
+        return claims.first { claim -> noOverlap(claim, fabric) }.id
+    }
 
-		val fabric = ShortArray(1_000_000)
+    private fun parseInput(input: String): Sequence<Claim> {
+        val rule = "#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)".toRegex()
+        return input.splitToSequence("\n")
+            .map { rule.matchEntire(it)!!.destructured }
+            .map { (id, x, y, w, h) -> Claim(id, x.toInt(), y.toInt(), w.toInt(), h.toInt()) }
+    }
 
-		claims.forEach { claim ->
-			for (x in claim.x until claim.x + claim.w)
-				for (y in claim.y until claim.y + claim.h)
-					fabric[1000 * y + x]++
+    private fun applyClaimsToFabric(claims: Sequence<Claim>) =
+        ShortArray(1_000_000).apply {
+            claims.forEach { claim ->
+                for (x in claim.x until claim.x + claim.w)
+                    for (y in claim.y until claim.y + claim.h)
+                        this[1000 * y + x]++
+            }
+        }
 
-		}
-
-		return fabric.count { it > 1 }.toString()
-	}
-
-	override fun solvePartTwo(input: String): String {
-		val rule = "#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)".toRegex()
-		val claims = input.splitToSequence("\n")
-				.map { rule.matchEntire(it)!!.destructured }
-				.map { (id, x, y, w, h) -> Fabric(id, x.toInt(), y.toInt(), w.toInt(), h.toInt()) }
-
-		val fabric = ShortArray(1_000_000)
-
-		claims.forEach { claim ->
-			for (x in claim.x until claim.x + claim.w)
-				for (y in claim.y until claim.y + claim.h)
-					fabric[1000 * y + x]++
-
-		}
-
-		return claims.first { claim ->
-			for (x in claim.x until claim.x + claim.w)
-				for (y in claim.y until claim.y + claim.h)
-					if (fabric[1000 * y + x] > 1) return@first false
-			true
-		}.id
-	}
+    private fun noOverlap(claim: Claim, fabric: ShortArray): Boolean {
+        for (x in claim.x until claim.x + claim.w)
+            for (y in claim.y until claim.y + claim.h)
+                if (fabric[1000 * y + x] > 1) return false
+        return true
+    }
 }
 
-private data class Fabric(
-		val id: String,
-		val x: Int,
-		val y: Int,
-		val w: Int,
-		val h: Int)
+private data class Claim(
+    val id: String,
+    val x: Int,
+    val y: Int,
+    val w: Int,
+    val h: Int)
