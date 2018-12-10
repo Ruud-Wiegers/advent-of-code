b7c4bfca0436ee870c7ad11da381f5a69f8878fe
Ruud Wiegers <ruud.wiegers@topicus.nl>
2018-12-10 09:17:13 +0100

    Day 10, minder lelijk.

--- src/adventofcode/InputFetcher.kt
+++ src/adventofcode/InputFetcher.kt
@@ -27,6 +27,8 @@ private fun readfromUrl(day: Int, year: Int): String {
 			.trimEnd()
 }
 
+private const val AOC_SESSION: String = "53616c7465645f5f4f0823a1d4515de6908449cc82531d705adac0dc54b9d3e2beda3e5dcda39a9992feacc6a3bc2a2f"
+
 private fun write(input: String, path: String) {
 	File(path).writeText(input)
 }
--- src/adventofcode/y2018/Day10.kt
+++ src/adventofcode/y2018/Day10.kt
@@ -8,46 +8,50 @@ fun main() = Day10.solve()
 
 object Day10 : AdventSolution(2018, 10, "The Stars Align") {
 
-    override fun solvePartOne(input: String) {
-        parse(input).let { solve(it) }
+    override fun solvePartOne(input: String) = parse(input).findMessage().value.map(Light::p).toList().asMessage()
+
+    override fun solvePartTwo(input: String) = parse(input).findMessage().index
+
+    private fun List<Light>.findMessage(): IndexedValue<List<Light>> {
+        return generateSequence(this) { it.map(Light::next) }
+                .withIndex().dropWhile { it.value.any { !it.isVisible() } }
+                .first { it.value.all { a -> it.value.any { other -> a.adjacent(other) } } }
     }
 
-    override fun solvePartTwo(input: String) = parse(input).let { "" }
-
-
     private fun parse(input: String) = input
-            .splitToSequence("\n").map { line ->
-                line
+            .splitToSequence("\n")
+            .map {
+                it
                         .split("position=<", ",", "> velocity=<", ">")
-                        .map { it.trim() }
-                        .filter { it.isNotBlank() }
-                        .map { it.toInt() }
+                        .map(String::trim)
+                        .filter(String::isNotBlank)
+                        .map(String::toInt)
             }
-            .map { (px, py, vx, vy) -> Point(px, py) to Point(vx, vy) }
+            .map { (px, py, vx, vy) -> Light(px, py, vx, vy) }
             .toList()
+
+    private fun List<Point>.asMessage(): String {
+        val x0 = minBy { it.x }!!.x
+        val x1 = maxBy { it.x }!!.x
+        val y0 = minBy { it.y }!!.y
+        val y1 = maxBy { it.y }!!.y
+
+        val grid = List(y1 - y0 + 1) { CharArray(x1 - x0 + 1) { ' ' } }
+
+        forEach { (x, y) -> grid[y - y0][x - x0] = '#' }
+
+        return grid.joinToString("") { "\n" + String(it) }
+    }
 }
 
-private fun solve(input: List<Pair<Point, Point>>) {
-    val sky = generateSequence (input){it.map { (p, v) -> p + v to v }}.map { it.map { it.first } }
+private data class Light(val p: Point, val v: Point) {
+    constructor(px: Int, py: Int, vx: Int, vy: Int) : this(Point(px, py), Point(vx, vy))
 
-    val region = sky.withIndex().dropWhile { it.value.any {  p -> p.x < 0 || p.y < 0 }}
-            .takeWhile {  it.value.all {  p -> p.x >= 0 || p.y >= 0 }}
-            .maxBy { it.value.sumBy { a-> it.value.count { b-> a.adjacent(b) } } }!!
-    println(region.index)
-for (y in 105..117) {
-    for (x in 105..170)
-        if (Point(x, y) in region.value)
-            print("#")
-        else
-            print(" ")
-    println()
-}
-
+    fun next() = Light(p + v, v)
+    fun isVisible() = p.x >= 0 && p.y >= 0
+    fun adjacent(o: Light) = abs(p.x - o.p.x) + abs(p.y - o.p.y) in 1..2
 }
 
 private data class Point(val x: Int, val y: Int) {
     operator fun plus(o: Point) = Point(x + o.x, y + o.y)
-    fun adjacent(o:Point) = abs(x-o.x) + abs(y-o.y) <2
 }
-
-
