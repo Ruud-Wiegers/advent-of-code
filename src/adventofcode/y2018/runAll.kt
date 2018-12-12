dc71f079f885104801cbb7c4674719942f5b00ec
Ruud Wiegers <ruud.wiegers@topicus.nl>
2018-12-12 08:47:47 +0100

    Day 12, part two.

--- src/adventofcode/y2018/runAll.kt
+++ src/adventofcode/y2018/runAll.kt
@@ -4,8 +4,9 @@ import adventofcode.AdventSolution
 import adventofcode.solve
 
 fun main(args: Array<String>) {
-    repeat(5) {
-        listOf(Day01, Day02, Day03, Day04, Day05, Day06, Day07, Day08, Day09, Day10, Day11)
+        listOf(
+                Day01, Day02, Day03, Day04, Day05,
+                Day06, Day07, Day08, Day09, Day10,
+                Day11, Day12)
                 .forEach(AdventSolution::solve)
-    }
 }
