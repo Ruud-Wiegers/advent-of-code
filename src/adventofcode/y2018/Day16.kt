f8c40c29fdd5479b3c13ea19438a854245b17e98
Ruud Wiegers <ruud.wiegers@topicus.nl>
2018-12-16 19:27:51 +0100

    Fixed 16

--- src/adventofcode/y2018/Day16.kt
+++ src/adventofcode/y2018/Day16.kt
@@ -1,77 +1,103 @@
 package adventofcode.y2018
 
 import adventofcode.AdventSolution
-import adventofcode.solve
-
-fun main(args: Array<String>) {
-    Day16.solve()
-}
 
 object Day16 : AdventSolution(2018, 16, "Chronal Classification") {
-    override fun solvePartOne(input: String) = parse(input).first.count { allOperations.count(it::isSolvedBy) >= 3 }
+
+    override fun solvePartOne(input: String) =
+            parse(input).first.count { example ->
+                allOperations.count { operation -> operation.solves(example) } >= 3
+            }
 
     override fun solvePartTwo(input: String): Int {
         val (examples, program) = parse(input)
-        val foundOpcodes = findOperationsFromExamples(examples)
+        val operations = findOperationsFromExamples(examples)
 
-        val result = program.fold(listOf(0, 0, 0, 0)) { state, instruction ->
-            foundOpcodes[instruction.opcode].execute(state, instruction)
+        val initial = listOf(0, 0, 0, 0)
+        val result = program.fold(initial) { registers, instruction ->
+            operations[instruction.opcode].execute(registers, instruction)
         }
+
         return result[0]
     }
 
-    private fun findOperationsFromExamples(examples: List<Example>): List<Operation> {
-        val unknownOpcodes = allOperations.indices.associateWith { allOperations.toMutableSet() }.toSortedMap()
-        val foundOpcodes = sortedMapOf<Int,Operation>()
-        while (unknownOpcodes.isNotEmpty()) {
-            var count = 0
-            examples
-                    .asSequence()
-                    .takeWhile { unknownOpcodes.isNotEmpty() }
-                    .filter { unknownOpcodes[it.instruction.opcode] != null }
-                    .forEach { example ->
-                        val opcode = example.instruction.opcode
-                        val ops = unknownOpcodes[opcode]!!
-                        ops.retainAll { example.isSolvedBy(it) }
-                        if (ops.size == 1) {
-                            val operation = ops.first()
-                            unknownOpcodes.remove(example.instruction.opcode)
-                            unknownOpcodes.values.forEach { it.remove(operation) }
-                            foundOpcodes[example.instruction.opcode] = operation
-                            println(count)
-                        }
-                        count++
+    private fun findOperationsFromExamples(examples: Sequence<Example>): List<Operation> {
+        val ambiguousOpcodes = allOperations.indices.associateWith { allOperations.toMutableSet() }.toSortedMap()
+        val foundOpcodes = sortedMapOf<Int, Operation>()
+
+        examples
+                .takeWhile { ambiguousOpcodes.isNotEmpty() }
+                .forEach { example ->
+
+                    //the opcode of the example can only refer to operations that solves the example
+                    ambiguousOpcodes[example.instruction.opcode]?.retainAll { it.solves(example) }
+
+                    //remove all found opcodes/operations from the search.
+                    //this can result in determining other opcodes/operations, so iterate.
+                    while (ambiguousOpcodes.any { it.value.size == 1 }) {
+
+                        //find all determined opcodes
+                        ambiguousOpcodes.keys.filter { ambiguousOpcodes[it]!!.size == 1 }
+                                .forEach { opCode ->
+                                    //move to found codes
+                                    val operation = ambiguousOpcodes.remove(opCode)!!.first()
+                                    foundOpcodes[opCode] = operation
+
+                                    //no other opcode can refer to this operator
+                                    ambiguousOpcodes.values.forEach { it -= operation }
+                                }
                     }
-        }
+                }
+
+        //the map is sorted by opcode, so this results in a correctly indexed list of operators
         return foundOpcodes.values.toList()
     }
 
-    private fun parse(input: String): Pair<List<Example>, List<Instruction>> {
-        val (ex, prog) = input.split("\n\n\n\n")
-        val examples = ex.split("\n\n")
+    private fun parse(input: String): Pair<Sequence<Example>, Sequence<Instruction>> {
+        val (partOne, partTwo) = input.split("\n\n\n\n")
 
-        val e = examples.map {
-            val (before, op, after) = it.split("\n").map { line ->
-                line.filter { c -> c in ('0'..'9') || c == ' ' }
-                        .trim()
-                        .split(" ")
-                        .map(String::toInt)
-            }
-            Example(
-                    before,
-                    Instruction(op[0], op[1], op[2], op[3]),
-                    after
-            )
+        val examples = partOne.splitToSequence("\n\n")
+                .map {
+                    val (before, op, after) = it.split("\n").map { line ->
+                        line.filter { c -> c in ('0'..'9') || c == ' ' }
+                                .trim()
+                                .split(" ")
+                                .map(String::toInt)
+                    }
+                    Example(
+                            before,
+                            Instruction(op[0], op[1], op[2], op[3]),
+                            after
+                    )
 
-        }
+                }
 
-        val p = prog.split('\n').map { it.split(" ").map(String::toInt).let { (a, b, c, d) -> Instruction(a, b, c, d) } }
-        return (e to p)
+        val program = partTwo.splitToSequence('\n')
+                .map {
+                    it.split(" ")
+                            .map(String::toInt)
+                            .let { (a, b, c, d) -> Instruction(a, b, c, d) }
+                }
+
+        return (examples to program)
     }
 }
 
+private data class Example(val before: Registers, val instruction: Instruction, val after: Registers)
+private data class Instruction(val opcode: Int, val input1: Int, val input2: Int, val target: Int)
+private typealias Registers = List<Int>
+
 private typealias Operation = (r: Registers, a: Int, b: Int) -> Int
 
+private fun Operation.execute(registers: Registers, instruction: Instruction): Registers =
+        registers.toMutableList().also { regs ->
+            regs[instruction.target] = this(registers, instruction.input1, instruction.input2)
+        }
+
+private fun Operation.solves(example: Example): Boolean =
+        execute(example.before, example.instruction) == example.after
+
+//sadly, there's no concise way to name these
 private val allOperations = listOf<Operation>(
         { r, a, b -> r[a] + r[b] },
         { r, a, b -> r[a] + b },
@@ -90,18 +116,3 @@ private val allOperations = listOf<Operation>(
         { r, a, b -> if (r[a] == b) 1 else 0 },
         { r, a, b -> if (r[a] == r[b]) 1 else 0 }
 )
-
-
-private fun Operation.execute(registers: Registers, instruction: Instruction): Registers =
-        registers.toMutableList().also { regs ->
-            regs[instruction.target] = this(registers, instruction.input1, instruction.input2)
-        }
-
-
-private data class Example(val before: Registers, val instruction: Instruction, val after: Registers) {
-    fun isSolvedBy(op: Operation) = op.execute(before, instruction) == after
-}
-
-private typealias Registers = List<Int>
-
-private data class Instruction(val opcode: Int, val input1: Int, val input2: Int, val target: Int)
