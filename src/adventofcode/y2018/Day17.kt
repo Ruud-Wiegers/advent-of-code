0bd0ff2b8e6289ab8a557aea0adfde0ea771601a
Ruud Wiegers <ruud.wiegers@topicus.nl>
2018-12-18 19:48:38 +0100

    Fiddling with day 17

--- src/adventofcode/y2018/Day17.kt
+++ src/adventofcode/y2018/Day17.kt
@@ -1,72 +1,98 @@
 package adventofcode.y2018
 
 import adventofcode.AdventSolution
+import adventofcode.solve
 
+fun main() = Day17.solve()
 
 object Day17 : AdventSolution(2018, 17, "Reservoir Research") {
 
-    override fun solvePartOne(input: String): Int {
-        val blocked = parse(input)
-        val flowingWater = mutableSetOf(Point(500, 0))
-        val stillWater = mutableSetOf<Point>()
+    override fun solvePartOne(input: String) =
+            Reservoirs(input).apply { this.flow(Point(500, 0)) }.countWater()
 
-        val yRange = blocked.minBy { it.y }!!.y..blocked.maxBy { it.y }!!.y
+    override fun solvePartTwo(input: String) =
+            Reservoirs(input).apply { this.flow(Point(500, 0)) }.countStill()
 
-        do {
-            var changes = false
-            for (w in flowingWater.toList()) {
-                val down = w.copy(y = w.y + 1)
-                if (down.y > yRange.last) continue
-                if (down !in blocked) {
-                    val add = flowingWater.add(down)
-                    changes = changes || add
-                } else {
-                    val left = w.copy(x = w.x - 1)
-                    if (left !in blocked) {
-                        val add = flowingWater.add(left)
-                        changes = changes || add
-                    } else {
-                        val xs = generateSequence(w.x) { it + 1 }.takeWhile { w.copy(x = it) in flowingWater }.toList()
-                        if (xs.isNotEmpty() && w.copy(x = xs.last() + 1) in blocked && xs.all { Point(it, w.y + 1) in blocked }) {
-                            xs.map { w.copy(x = it) }
-                                    .forEach {
-                                        blocked.add(it)
-                                        stillWater.add(it)
-                                        flowingWater.remove(it)
-                                    }
-                            changes = true
-                        }
-                    }
+    private class Reservoirs(input: String) {
+        val xRange: IntRange
+        val yRange: IntRange
+        val map: List<CharArray>
 
-                    val right = w.copy(x = w.x + 1)
-                    if (right !in blocked) {
-                        val add = flowingWater.add(right)
-                        changes = changes || add
-                    } else {
-                        val xs = generateSequence(w.x) { it - 1 }.takeWhile { w.copy(x = it) in flowingWater }.toList()
-                        if (xs.isNotEmpty() && w.copy(x = xs.last() + 1) in blocked && xs.all { Point(it, w.y + 1) in blocked }) {
-                            xs.map { w.copy(x = it) }
-                                    .forEach {
-                                        blocked.add(it)
-                                        stillWater.add(it)
-                                        flowingWater.remove(it)
-                                    }
-                            changes = true
-                        }
-                    }
-                }
+        init {
+            val clay = input.let(::parseToClayCoordinates)
+
+            xRange = clay.minBy { it.x }!!.x..clay.maxBy { it.x }!!.x
+            yRange = clay.minBy { it.y }!!.y..clay.maxBy { it.y }!!.y
+            map = coordinatesToMap(clay)
+
+        }
+
+        fun flow(p: Point) {
+            val d = Point(p.x, p.y + 1)
+            if (d !in map) return
+
+            if (map[d] == '.') {
+                map[d] = '|'
+                flow(d)
             }
 
-        } while (changes)
-        println((stillWater).count { it.y in yRange })
+            val l = Point(p.x - 1, p.y)
+            if (map[d] in "#~" && l in map && map[l] == '.') {
+                map[l] = '|'
+                flow(l)
+            }
 
-        return (flowingWater + stillWater).count { it.y in yRange }
+            val r = Point(p.x + 1, p.y)
+            if (map[d] in "#~" && r in map && map[r] == '.') {
+                map[r] = '|'
+                flow(r)
+            }
+
+            if (hasWalls(p)) fillLeftAndRight(p)
+        }
+
+        private fun hasWalls(source: Point): Boolean =
+                hasWall(source.x downTo 0, source.y) && hasWall(source.x..xRange.last, source.y)
+
+        private fun hasWall(xs: IntProgression, y: Int): Boolean {
+
+            for (x in xs)
+                when (map[y][x]) {
+                    '#' -> return true
+                    '.' -> return false
+                }
+            return false
+        }
+
+        private fun fillLeftAndRight(source: Point) {
+            fillUntilWall(source.x downTo 0, source.y)
+            fillUntilWall(source.x..xRange.last, source.y)
+        }
+
+        private fun fillUntilWall(xs: IntProgression, y: Int) {
+            for (x in xs) {
+                if (map[y][x] == '#') return
+                map[y][x] = '~'
+            }
+        }
+
+        private fun coordinatesToMap(clay: Set<Point>): List<CharArray> {
+
+            val map = List(yRange.last + 1) {
+                CharArray(xRange.last + 1) { '.' }
+            }
+
+            clay.forEach { (x, y) -> map[y][x] = '#' }
+            map[0][500] = '+'
+            return map
+        }
+
+        fun countWater() = map.slice(yRange).sumBy { it.count { it in "|~" } }
+        fun countStill() = map.slice(yRange).sumBy { it.count { it == '~' } }
 
     }
 
-    override fun solvePartTwo(input: String) = ""
-
-    private fun parse(input: String): MutableSet<Point> {
+    private fun parseToClayCoordinates(input: String): MutableSet<Point> {
         val clay = mutableSetOf<Point>()
         val regex = "(.).*?(\\d+).*?(\\d+).*?(\\d+).*?".toRegex()
         input.splitToSequence("\n")
@@ -81,4 +107,11 @@ object Day17 : AdventSolution(2018, 17, "Reservoir Research") {
     }
 
     data class Point(val x: Int, val y: Int)
+
+}
+
+private operator fun List<CharArray>.contains(p: Day17.Point) = p.y in indices && p.x in this[0].indices
+private operator fun List<CharArray>.get(p: Day17.Point) = this[p.y][p.x]
+private operator fun List<CharArray>.set(p: Day17.Point, c: Char) {
+    this[p.y][p.x] = c
 }
