e69dac554d5a868854045f7e498b5eee6e2eafb5
Ruud Wiegers <ruud.wiegers@topicus.nl>
2019-10-30 08:57:26 +0100

    cleanup day 13

--- src/adventofcode/y2016/Day13.kt
+++ src/adventofcode/y2016/Day13.kt
@@ -6,44 +6,42 @@ object Day13 : AdventSolution(2016, 13, "Maze") {
 
 	var magicNumber: Int = 0
 
-	override fun solvePartOne(input: String): String {
+	override fun solvePartOne(input: String): Int {
 		magicNumber = input.toInt()
-		var openList = setOf(MazeState(1, 1))
-		val closedList = mutableSetOf<MazeState>()
-		var count = 0
-		while (openList.none { it.x == 31 && it.y == 39 }) {
-			val new = openList.asSequence().flatMap(MazeState::getNeighbors).filterNot { it in closedList }.toSet()
-			closedList += openList
-			openList = new
-			count++
-		}
-		return count.toString()
+
+		return generateSequence(Solver(setOf(Cubicle(1, 1))), Solver::next)
+				.takeWhile { Cubicle(31, 39) !in it.edge }
+				.count()
 	}
 
-	override fun solvePartTwo(input: String): String {
+	override fun solvePartTwo(input: String): Int {
 		magicNumber = input.toInt()
-		var openList = setOf(MazeState(1, 1))
-		val closedList = mutableSetOf<MazeState>()
-		repeat(50) {
-			val new = openList.asSequence().flatMap(MazeState::getNeighbors).filterNot { it in closedList }.toSet()
-			closedList += openList
-			openList = new
-		}
-		return (closedList.size + openList.size).toString()
+
+		return generateSequence(Solver(setOf(Cubicle(1, 1))), Solver::next)
+				.take(52)
+				.last()
+				.visited
+				.size
 	}
 
 
-	data class MazeState(val x: Int, val y: Int) {
+	data class Solver(val edge: Set<Cubicle>, val visited: Set<Cubicle> = emptySet()) {
+		fun next() = Solver(
+				edge = edge.asSequence().flatMap(Cubicle::getNeighbors).filterNot { it in visited }.toSet(),
+				visited = visited + edge
+		)
+	}
+
+	data class Cubicle(val x: Int, val y: Int) {
 		fun getNeighbors() = sequenceOf(
 				copy(x = x + 1),
 				copy(x = x - 1),
 				copy(y = y + 1),
 				copy(y = y - 1))
-				.filter(MazeState::isValid)
+				.filter(Cubicle::isOpen)
 
-		private fun isValid(): Boolean = x >= 0
+		private fun isOpen(): Boolean = x >= 0
 				&& y >= 0
 				&& Integer.bitCount(x * x + 3 * x + 2 * x * y + y + y * y + magicNumber) % 2 == 0
-
 	}
 }
