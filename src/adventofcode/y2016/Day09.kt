96b5c810b9603d21b67b6bc9d88254cac1f6a549
Ruud Wiegers <ruud.wiegers@topicus.nl>
2019-11-15 19:34:39 +0100

    2016, day 9, much cleaner.

--- src/adventofcode/y2016/Day09.kt
+++ src/adventofcode/y2016/Day09.kt
@@ -3,45 +3,16 @@ package adventofcode.y2016
 import adventofcode.AdventSolution
 
 object Day09 : AdventSolution(2016, 9, "Explosives in Cyberspace") {
-	override fun solvePartOne(input: String) = decompress(input).toString()
 
-	override fun solvePartTwo(input: String) = decompress2(input).toString()
+	private val rule = """(.*?)\((\d+)x(\d+)\)(.*)""".toRegex()
 
-
-	private fun decompress(content: String): Int {
-		var decompressedLength = 0
-
-		var pos = 0
-		val rule = ".*?((\\d+)x(\\d+)).*?".toRegex()
-		while (pos < content.length) {
-			val match = rule.matchEntire(content.subSequence(pos until content.length))
-					?: return decompressedLength + content.length - pos
-
-			val (_, b, r) = match.destructured
-			val indexOfCompressedBlock = match.groups[1]!!.range.last + 2
-			decompressedLength += b.toInt() * r.toInt()
-			decompressedLength += match.groups[1]!!.range.first -1
-			pos += indexOfCompressedBlock + b.toInt()
-		}
-		return decompressedLength
+	override fun solvePartOne(input: String): Int {
+		val (p, b, r, u) = rule.matchEntire(input)?.destructured ?: return input.length
+		return p.length + r.toInt() * b.toInt() + solvePartOne(u.drop(b.toInt()))
 	}
 
-	private fun decompress2(content: String): Long {
-		var decompressedLength = 0L
-
-		var pos = 0
-		val rule = ".*?((\\d+)x(\\d+)).*?".toRegex()
-		while (pos < content.length) {
-			val match = rule.matchEntire(content.subSequence(pos until content.length))
-					?: return decompressedLength + content.length - pos
-
-			val (_, b, r) = match.destructured
-			val indexOfCompressedBlock = match.groups[1]!!.range.last + 2
-			val substring = content.substring(pos + indexOfCompressedBlock, pos + indexOfCompressedBlock + b.toInt())
-			decompressedLength += match.groups[1]!!.range.first -1
-			decompressedLength += decompress2(substring) * r.toInt()
-			pos += indexOfCompressedBlock + b.toInt()
-		}
-		return decompressedLength
+	override fun solvePartTwo(input: String): Long {
+		val (p, b, r, u) = rule.matchEntire(input)?.destructured ?: return input.length.toLong()
+		return p.length + r.toInt() * solvePartTwo(u.take(b.toInt())) + solvePartTwo(u.drop(b.toInt()))
 	}
 }
