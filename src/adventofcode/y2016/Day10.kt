e032d8347d10bd96186bd82e0e2e8f17e6ce78ce
Ruud Wiegers <ruud.wiegers@topicus.nl>
2019-11-16 08:47:01 +0100

    cleaner robots

--- src/adventofcode/y2016/Day10.kt
+++ src/adventofcode/y2016/Day10.kt
@@ -3,98 +3,62 @@ package adventofcode.y2016
 import adventofcode.AdventSolution
 
 object Day10 : AdventSolution(2016, 10, "Balance Bots") {
-	override fun solvePartOne(input: String): String {
-		val (chips, instructionsMap) = parse(input)
+    override fun solvePartOne(input: String): Int? {
+        val (chips, lowInstr, highInstr) = parse(input)
 
+        while (chips.values.any { it.size > 1 }) {
+            step(chips, lowInstr, highInstr)
+            chips.entries.find { it.value == listOf(17, 61) }
+                    ?.let { return it.key }
+        }
+        return null
+    }
 
-		while (chips.values.any { it.size > 1 }) {
-			chips
-					.filter { it.value.size > 1 }
-					.forEach { (bot, ch) ->
-						chips.replace(bot, emptySet())
+    override fun solvePartTwo(input: String): Int {
+        val (chips, lowInstr, highInstr) = parse(input)
 
+        while (chips.values.any { it.size > 1 })
+            step(chips, lowInstr, highInstr)
 
-						val lowChip = minOf(ch.first(), ch.last())
-						val lowBot = instructionsMap[bot]?.lowId ?: 0
-						if (instructionsMap[bot]?.lowIsRobot == true) {
-							chips.merge(lowBot, setOf(lowChip)) { a, b -> a + b }
-						}
+        return (0..2)
+                .map { -it - 1 }
+                .map(chips::getValue)
+                .map(List<Int>::first)
+                .reduce(Int::times)
+    }
 
-						val highChip = maxOf(ch.first(), ch.last())
-						val highBot = instructionsMap[bot]?.highId ?: 0
-						if (instructionsMap[bot]?.highIsRobot == true) {
-							chips.merge(highBot, setOf(highChip)) { a, b -> a + b }
-						}
-					}
+	private fun step(chips: MutableMap<Int, List<Int>>, lowInstr: Map<Int, Int>, highInstr: Map<Int, Int>) {
+        chips
+                .filter { it.value.size > 1 }
+                .forEach { (bot, ch) ->
+                    chips[bot] = emptyList()
 
+                    val (lowChip, highChip) = ch
 
-			chips.entries.find { 61 in it.value && 17 in it.value }?.let { return it.key.toString() }
-		}
-		return "???"
-	}
+                    chips.merge(lowInstr.getValue(bot), listOf(lowChip)) { a, b -> (a + b).sorted() }
+                    chips.merge(highInstr.getValue(bot), listOf(highChip)) { a, b -> (a + b).sorted() }
+                }
+    }
 
+    private fun parse(input: String): Triple<MutableMap<Int, List<Int>>, Map<Int, Int>, Map<Int, Int>> {
+        val lines = input.splitToSequence("\n")
 
-	override fun solvePartTwo(input: String): String {
+        val valuePattern = ("value (\\d+) goes to bot (\\d+)").toRegex()
+        val chips = lines
+                .mapNotNull { valuePattern.matchEntire(it) }
+                .groupBy({ it.groupValues[2].toInt() }, { it.groupValues[1].toInt() })
+                .mapValues { it.value.sorted() }
+                .toMutableMap()
 
-		val (chips, instructionsMap) = parse(input)
+        val instructionPattern = ("bot (\\d+) gives low to (output|bot) (\\d+) and high to (output|bot) (\\d+)").toRegex()
 
-		val outputs = mutableMapOf<Int, Int>()
+        val low = lines
+                .mapNotNull { instructionPattern.matchEntire(it)?.destructured }
+                .associate { (b0, t1, b1) -> b0.toInt() to if (t1 == "bot") b1.toInt() else -b1.toInt() - 1 }
 
-		while (chips.values.any { it.size > 1 }) {
-			chips
-					.filter { it.value.size > 1 }
-					.forEach { (bot, ch) ->
-						chips.replace(bot, emptySet())
-
-
-						val lowChip = minOf(ch.first(), ch.last())
-						val lowBot = instructionsMap[bot]?.lowId ?: 0
-						if (instructionsMap[bot]?.lowIsRobot == true) {
-							chips[lowBot] = chips[lowBot].orEmpty() + lowChip
-						} else {
-							outputs[lowBot] = lowChip
-						}
-
-						val highChip = maxOf(ch.first(), ch.last())
-						val highBot = instructionsMap[bot]?.highId ?: 0
-						if (instructionsMap[bot]?.highIsRobot == true) {
-							chips[highBot] = chips[highBot].orEmpty() + highChip
-						} else {
-							outputs[highBot] = highChip
-						}
-					}
-		}
-		return (0..2).map(outputs::getValue).reduce(Int::times).toString()
-	}
-
-	private fun parse(input: String): Pair<MutableMap<Int, Set<Int>>, Map<Int, BotInstruction>> {
-		val (valueStrings, instructionStrings) = input.splitToSequence("\n").partition { it.startsWith("value") }
-
-		val chips: MutableMap<Int, Set<Int>> = mutableMapOf()
-
-		val valuePattern = ("value (\\d+) goes to bot (\\d+)").toRegex()
-		valueStrings
-				.map { valuePattern.matchEntire(it)!!.destructured }
-				.map { (ch, bot) -> ValueInstruction(ch.toInt(), bot.toInt()) }
-				.forEach { chips[it.receiver] = chips[it.receiver].orEmpty() + it.chip }
-
-		val instructionPattern = ("bot (\\d+) gives low to (output|bot) (\\d+) and high to (output|bot) (\\d+)").toRegex()
-		val instructionsMap = instructionStrings
-				.map { instructionPattern.matchEntire(it)!!.destructured }
-				.map { (b0, t1, b1, t2, b2) -> BotInstruction(b0.toInt(), t1 == "bot", b1.toInt(), t2 == "bot", b2.toInt()) }
-				.associate { it.id to it }
-		return Pair(chips, instructionsMap)
-	}
+        val high = lines
+                .mapNotNull { instructionPattern.matchEntire(it)?.destructured }
+                .associate { (b0, _, _, t1, b1) -> b0.toInt() to if (t1 == "bot") b1.toInt() else -b1.toInt() - 1 }
+        return Triple(chips, low, high)
+    }
 }
-
-sealed class Instruction
-data class ValueInstruction(
-		val chip: Int,
-		val receiver: Int) : Instruction()
-
-data class BotInstruction(
-		val id: Int,
-		val lowIsRobot: Boolean,
-		val lowId: Int,
-		val highIsRobot: Boolean,
-		val highId: Int) : Instruction()
