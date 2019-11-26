f6d2091b5c67edb2695bc8a325921a31591de393
Ruud Wiegers <ruud.wiegers@topicus.nl>
2019-11-26 19:18:38 +0100

    cleaner 2018 day 20

--- src/adventofcode/y2018/Day20.kt
+++ src/adventofcode/y2018/Day20.kt
@@ -5,88 +5,51 @@ import java.util.*
 
 object Day20 : AdventSolution(2018, 20, "A Regular Map") {
 
-    override fun solvePartOne(input: String): Int {
-        val neighbors = parseToNeighbors(input)
+    override fun solvePartOne(input: String) = findDistances(input).values.max()
 
-        //BFS
-        var open = setOf(Point(0, 0))
-        val closed = mutableSetOf<Point>()
-        var count = 0
-        while (open.isNotEmpty()) {
-            val new = open.map { neighbors[it]!! }.reduce { a, b -> a + b } - closed
-            closed += new
-            open = new
-            count++
-        }
-        return count - 1
-    }
+    override fun solvePartTwo(input: String) =
+            findDistances(input).count { it.value >= 1000 }
 
-    override fun solvePartTwo(input: String): Int {
-        val neighbors = parseToNeighbors(input)
-
-        //BFS
-        var open = setOf(Point(0, 0))
-        val closed = mutableSetOf<Point>()
-        repeat(999) {
-            val new = open.map { neighbors[it]!! }.reduce { a, b -> a + b } - closed
-            closed += new
-            open = new
-        }
-
-        return (neighbors.keys - closed).size
-    }
-
-    private fun parseToNeighbors(input: String): Map<Point, Set<Point>> {
-        val neighbors = mutableMapOf<Point, MutableSet<Point>>()
-
-        //the positions of the current branches we're following
-        val restartOnAlternative = ArrayDeque<Set<Point>>()
-
-        //endpoints of subbranches (from which we must continue)
-        val continueAfterBranch = ArrayDeque<Set<Point>>()
+    private fun findDistances(input: String): Map<Point, Int> {
+        val distanceTo = mutableMapOf(Point(0, 0) to 0)
         var currentPositions = setOf(Point(0, 0))
+        val positionsWhenEnteringIntersection = Stack<Set<Point>>()
+        val endPositionsOfBranches = Stack<Set<Point>>()
+
 
         for (ch in input) {
             when (ch) {
                 in "NESW" -> {
-                    //This demonstrates that the input is waaaay nicer than the problem description requires
-                    //With this assumption, we would only need a single stack of single positions
-                    check(currentPositions.size == 1)
-
-                    val newPositions = mutableSetOf<Point>()
-                    currentPositions.forEach { room ->
-                        val n = room.step(ch)
-
-                        neighbors.getOrPut(room) { mutableSetOf() }.add(n)
-                        neighbors.getOrPut(n) { mutableSetOf() }.add(room)
-                        newPositions.add(n)
-                    }
-                    currentPositions = newPositions
+                    currentPositions = currentPositions.map { oldPosition ->
+                        oldPosition.step(ch).apply {
+                            distanceTo.merge(this, distanceTo.getValue(oldPosition) + 1, ::minOf)
+                        }
+                    }.toSet()
                 }
                 '(' -> {
-                    restartOnAlternative.push(currentPositions)
-                    continueAfterBranch.push(emptySet())
+                    endPositionsOfBranches.push(emptySet())
+                    positionsWhenEnteringIntersection.push(currentPositions)
                 }
                 '|' -> {
-                    continueAfterBranch.push(continueAfterBranch.pop() + currentPositions)
-                    currentPositions = restartOnAlternative.peek()
+                    endPositionsOfBranches.push(endPositionsOfBranches.pop() + currentPositions)
+                    currentPositions = positionsWhenEnteringIntersection.peek()
                 }
                 ')' -> {
-                    restartOnAlternative.pop()
-                    currentPositions = continueAfterBranch.pop() + currentPositions
+                    currentPositions = endPositionsOfBranches.pop() + currentPositions
+                    positionsWhenEnteringIntersection.pop()
                 }
             }
         }
-        return neighbors
+        return distanceTo
     }
 
-    private data class Point(val x: Int, val y: Int)
-
-    private fun Point.step(ch: Char) = when (ch) {
-        'N' -> Point(x, y - 1)
-        'E' -> Point(x + 1, y)
-        'S' -> Point(x, y + 1)
-        'W' -> Point(x - 1, y)
-        else -> throw IllegalStateException()
+    private data class Point(val x: Int, val y: Int) {
+        fun step(ch: Char) = when (ch) {
+            'N' -> Point(x, y - 1)
+            'E' -> Point(x + 1, y)
+            'S' -> Point(x, y + 1)
+            'W' -> Point(x - 1, y)
+            else -> throw IllegalStateException()
+        }
     }
 }
