9b1d2f54ac6eaa1f6568fbe3966a43f8efd5fdf5
Ruud Wiegers <ruud.wiegers@topicus.nl>
2019-12-06 12:44:08 +0100

    revert Day 6 part 1 to fast solution.

--- src/adventofcode/y2019/Day06.kt
+++ src/adventofcode/y2019/Day06.kt
@@ -8,25 +8,27 @@ fun main() = Day06.solve()
 object Day06 : AdventSolution(2019, 6, "Universal Orbit Map") {
 
     override fun solvePartOne(input: String): Int {
-        val orbits: Map<String, String> = parse(input)
+        val satellites: Map<String, List<String>> = input
+                .lineSequence()
+                .map { it.split(')') }
+                .groupBy({ it[0] }, { it[1] })
 
-        return orbits.keys.sumBy { satellite ->
-            generateSequence(satellite, orbits::get).count() - 1
-        }
+        fun check(obj: String, depth: Int): Int =
+                depth + satellites[obj].orEmpty().sumBy { check(it, depth + 1) }
+
+        return check("COM", 0)
     }
 
     override fun solvePartTwo(input: String): Int {
-        val orbits: Map<String, String> = parse(input)
+        val parents: Map<String, String> = input
+                .lineSequence()
+                .map { it.split(')') }
+                .associate { it[1] to it[0] }
 
-        val y = generateSequence("YOU", orbits::get).toList().asReversed()
-        val s = generateSequence("SAN", orbits::get).toList().asReversed()
+        val y = generateSequence("YOU", parents::get).toList().asReversed()
+        val s = generateSequence("SAN", parents::get).toList().asReversed()
 
         val same = y.zip(s).count { (a, b) -> a == b }
         return y.size + s.size - 2 * same - 2
     }
-
-    private fun parse(input: String) = input
-            .lineSequence()
-            .map { it.split(')') }
-            .associate { it[1] to it[0] }
 }
