9678b9da88d5598233bde50b0d2b6fa6bcd98af4
Ruud Wiegers <ruud.wiegers@topicus.nl>
2019-12-16 19:09:06 +0100

    day16

--- src/adventofcode/y2019/Day16.kt
+++ src/adventofcode/y2019/Day16.kt
@@ -3,14 +3,15 @@ package adventofcode.y2019
 import adventofcode.AdventSolution
 import adventofcode.solve
 import adventofcode.util.collections.cycle
+import adventofcode.util.collections.scan
 import kotlin.math.absoluteValue
 
 fun main() = Day16.solve()
 
-object Day16 : AdventSolution(2019, 16, "FFT") {
+object Day16 : AdventSolution(2019, 16, "Flawed Frequency Transmission") {
 
     override fun solvePartOne(input: String) =
-            generateSequence(input.map { '0' - it }) { fft(it) }
+            generateSequence(input.map { it - '0' }) { fft(it) }
                     .take(101)
                     .last()
                     .take(8)
@@ -30,6 +31,23 @@ object Day16 : AdventSolution(2019, 16, "FFT") {
             .cycle()
             .drop(1)
 
-    override fun solvePartTwo(input: String) = 0
+    override fun solvePartTwo(input: String): String {
+
+        val offset = input.take(7).toInt()
+
+        val repsToSkip = offset / input.length
+
+        val rr = repsToSkip until 10000
+
+        var res = rr.flatMap { input.map { it - '0' } }.reversed()
+
+        repeat(100) {
+            res = res.asSequence().scan(0, Int::plus).map { it % 10 }.toList()
+        }
+        res = res.reversed()
+
+        return res.drop(offset - (repsToSkip * input.length)).take(8).joinToString("")
+
+    }
 
 }
