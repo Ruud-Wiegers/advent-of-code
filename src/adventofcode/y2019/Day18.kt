0341a83517f7815dd29c4655bbd72da2c39b5998
Ruud Wiegers <ruud.wiegers@gmail.com>
2019-12-18 20:37:51 +0100

    alskdjfhjkasdflhkaskfd

--- src/adventofcode/y2019/Day18.kt
+++ src/adventofcode/y2019/Day18.kt
@@ -7,48 +7,69 @@ import adventofcode.util.vector.Vec2
 import adventofcode.util.vector.neighbors
 import java.util.*
 
-fun main() = Day18.solve()
+fun main() {
+    Day18.solvePartOne("""########################
+#...............b.C.D.f#
+#.######################
+#.....@.a.B.c.d.A.e.F.g#
+########################""")
+            .let(::println)
+}
 
 object Day18 : AdventSolution(2019, 18, "Many-Worlds Interpretation") {
+   private val alphabet = 'a'..'g'
 
-    override fun solvePartOne(input: String): Int? {
+    override fun solvePartOne(input: String): Map<Char, Int> {
         val (floor, objectAtLocation) = readMaze(input)
 
         val distancesWithClosedDoors: Map<Char, Map<Char, Int>> = generateDistanceMap(floor, objectAtLocation)
-        val keyDistancesWithOpenDoors: Map<Char, Map<Char, Int>> = generateDistanceMap(floor + objectAtLocation.keys, objectAtLocation.filterValues { it in ('a'..'z') + '@' })
+        val keyDistancesWithOpenDoors: Map<Char, Map<Char, Int>> = generateDistanceMap(floor + objectAtLocation.keys, objectAtLocation.filterValues { it in alphabet + '@' })
 
         val dependencies: Map<Char, Set<Char>> = buildDependencyTree(distancesWithClosedDoors)
         val keyRequirements = keyRequirements(dependencies)
 
-        val completion = (('a'..'z')+ '@').associateWith { mutableMapOf(emptySet<Char>() to 0) }
+        val keyRequiredBy = alphabet.associateWith { k -> alphabet.filter { k in keyRequirements[it]!! }.toSet() }
 
-        fun dp(pos: Char, missingKeys: Set<Char>): Int =
-                completion.getValue(pos).getOrPut(missingKeys) {
-                    missingKeys.asSequence().filter { keyRequirements[it]!!.none { it in missingKeys } }
-                            .map {
-                                val c = dp(it, missingKeys - it)
-                                c + keyDistancesWithOpenDoors.getValue(pos).getValue(it)
-                            }
-                            .min() ?: throw IllegalStateException()
-                }
-
-        return dp('@', ('a'..'z').toSet())
+        var completion = alphabet.associateWith { Pair(0, emptyList<Char>()) }
 
 
+        dependencies.forEach(::println)
+        keyRequirements.forEach(::println)
+
+
+        fun lookup(oldPos: Char): Pair<Char, Pair<Int, List<Char>>>? {
+            val required = keyRequirements[oldPos]!!
+            val requiredBy = keyRequiredBy[oldPos]!!
+
+            val startFrom = keyDistancesWithOpenDoors[oldPos]!!
+            return completion.filter { (_, nv) -> oldPos !in nv.second && required.none { it in nv.second } && requiredBy.all { it in nv.second } }
+                    .minBy { it.value.first + startFrom[it.key]!! }
+                    ?.let { oldPos to Pair(it.value.first + startFrom[it.key]!!, it.value.second + oldPos) }
+
+        }
+
+        //TODO it's possible to paint yourself into a corner, by
+
+        repeat(alphabet.last - alphabet.first) {
+            completion = alphabet.mapNotNull { oldPos -> lookup(oldPos) }.toMap()
+            completion.let(::println)
+
+        }
+
+        return completion.filter { it.key in dependencies['@']!! }.mapValues { keyDistancesWithOpenDoors['@']!![it.key]!! + it.value.first }
     }
 
     private fun keyRequirements(dependencies: Map<Char, Set<Char>>): Map<Char, SortedSet<Char>> {
         fun Map<Char, Set<Char>>.find(current: Char, target: Char): List<Char>? = when {
-            target in this[current]!! -> listOf()
+            target in this[current]!! -> listOf(current)
             else                      -> this[current]!!
-                    .filter { it in 'A'..'Z' }
+                    .filter { it in alphabet.map { it.toUpperCase() } }
                     .mapNotNull { this.find(it, target) }
                     .firstOrNull()
                     ?.let { it + current }
         }
 
-        val keyRequirements = ('a'..'z').associateWith { dependencies.find('@', it)!!.let { it - '@' }.toSortedSet() }
-        return keyRequirements
+        return alphabet.associateWith { dependencies.find('@', it)!!.let { it.map { it.toLowerCase() } - '@' }.toSortedSet() }
     }
 
 
@@ -69,7 +90,7 @@ object Day18 : AdventSolution(2019, 18, "Many-Worlds Interpretation") {
                 }
             }
         }
-        floor += objectAtLocation.filterValues { it !in 'A'..'Z' }.keys
+        floor += objectAtLocation.filterValues { it !in alphabet.map { it.toUpperCase() } }.keys
         return Pair(floor, objectAtLocation)
     }
 
@@ -112,7 +133,7 @@ object Day18 : AdventSolution(2019, 18, "Many-Worlds Interpretation") {
                 val new = distances[it]!!.keys.filter { it !in visited }.toSet()
                 tree += it to new
                 visited += new
-                new.filter { it in 'A'..'Z' }
+                new.filter { it in alphabet.map { it.toUpperCase() } }
             }.toSet()
         }
         return tree.toSortedMap()
