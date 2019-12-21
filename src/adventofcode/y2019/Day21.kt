b13896dd1a2457b5f3b3894d61a5fbcea45d8b2b
Ruud Wiegers <ruud.wiegers@topicus.nl>
2019-12-21 09:23:28 +0100

    Day 21 cleanup

--- src/adventofcode/y2019/Day21.kt
+++ src/adventofcode/y2019/Day21.kt
@@ -8,45 +8,33 @@ fun main() = Day21.solve()
 
 object Day21 : AdventSolution(2019, 21, "Springdroid") {
 
-    override fun solvePartOne(input: String): Long? {
+    override fun solvePartOne(input: String) = runDroid(input, listOf(
+            "OR A T",
+            "AND B T",
+            "AND C T",
+            "NOT T J",
+            "AND D J",
+            "WALK"))
+
+    override fun solvePartTwo(input: String) = runDroid(input, listOf(
+            "OR A T",
+            "AND B T",
+            "AND C T",
+            "NOT T J",
+            "AND D J",
+            "OR E T",
+            "OR H T",
+            "AND T J",
+            "RUN"))
+
+    private fun runDroid(input: String, springscript: List<String>): Long {
         val baseprogram = IntCodeProgram.fromData(input)
-
-        val springscript = listOf(
-                "OR A T",
-                "AND B T",
-                "AND C T",
-                "NOT T J",
-                "AND D J",
-                "WALK").joinToString("\n", postfix = "\n")
-
-        springscript.forEach { baseprogram.input(it.toLong()) }
+        springscript.joinToString("\n", postfix = "\n").forEach { baseprogram.input(it.toLong()) }
         baseprogram.execute()
-
-
         return generateSequence { baseprogram.output() }.last()
     }
 
-    private fun video(baseprogram: IntCodeProgram) {
-        generateSequence { baseprogram.output() }.map { it.toInt().toChar() }.joinToString("").let(::println)
-    }
-
-
-    override fun solvePartTwo(input: String): Long? {
-        val baseprogram = IntCodeProgram.fromData(input)
-
-        val springscript = listOf(
-                "OR A T",
-                "AND B T",
-                "AND C T",
-                "NOT T J",
-                "AND D J",
-                "OR H T",
-                "OR E T",
-                "AND T J",
-                "RUN").joinToString("\n", postfix = "\n")
-
-        springscript.forEach { baseprogram.input(it.toLong()) }
-        baseprogram.execute()
-        return generateSequence { baseprogram.output() }.last()
+    private fun IntCodeProgram.videoOut() {
+        generateSequence(this::output).map(Long::toChar).joinToString("").let(::println)
     }
 }
