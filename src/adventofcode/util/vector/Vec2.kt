b9b2837c8c1d4008358ba3254dcd54c3d82a4e00
Ruud Wiegers <ruud.wiegers@topicus.nl>
2019-12-21 07:39:44 +0100

    cleanup

--- src/adventofcode/util/vector/Vec2.kt
+++ src/adventofcode/util/vector/Vec2.kt
@@ -6,6 +6,7 @@ data class Vec2(val x: Int, val y: Int) {
     operator fun plus(o: Vec2) = Vec2(x + o.x, y + o.y)
     operator fun minus(o: Vec2) = Vec2(x - o.x, y - o.y)
     operator fun div(o: Int) = Vec2(x / o, y / o)
+    operator fun times(o: Int) = Vec2(x * o, y * o)
     fun distance(o: Vec2)=( o-this).let {  it.x.absoluteValue+it.y.absoluteValue}
 
     companion object {
--- src/adventofcode/y2019/Day20.kt
+++ src/adventofcode/y2019/Day20.kt
@@ -13,16 +13,21 @@ object Day20 : AdventSolution(2019, 20, "Donut maze") {
     override fun solvePartOne(input: String): Any? {
         val (floor, labels) = readMaze(input)
 
-        val portals = labelPortals(floor, labels)
+        val portals = labelPortals(floor, labels, Direction.RIGHT) + labelPortals(floor, labels, Direction.DOWN)
 
-        val portalNeighbors = portals.asSequence().mapNotNull { (entrance, entranceName) ->
-            portals.asSequence().find { (exit, exitName) -> entrance != exit && entranceName == exitName }?.key?.let {
-                entrance to it
-            }
-        }.toMap()
+        val portalNeighbors = portals
+                .asSequence()
+                .mapNotNull { (entrance, entranceName) ->
+                    portals.asSequence()
+                            .find { (exit, exitName) -> entrance != exit && entranceName == exitName }
+                            ?.key
+                            ?.let { entrance to it }
+                }
+                .toMap()
 
         val vs = Direction.values().map { it.vector }
-        fun neighbors(p: Vec2) = vs.map { it + p }.filter { it in floor } + portalNeighbors[p]?.let { listOf(it) }.orEmpty()
+        fun neighbors(p: Vec2) = vs.map { it + p }
+                .filter { it in floor } + portalNeighbors[p]?.let { listOf(it) }.orEmpty()
 
 
         val start = portals.asSequence().first { it.value == "AA" }.key
@@ -42,19 +47,10 @@ object Day20 : AdventSolution(2019, 20, "Donut maze") {
         return "failed"
     }
 
-    private fun labelPortals(floor: Set<Vec2>, labels: Map<Vec2, Char>): Map<Vec2, String> {
-        val horizontal: Map<Vec2, String> = labels
-                .filterKeys { k -> k + Direction.RIGHT.vector in labels }
-                .mapValues { (k, v) -> v.toString() + labels[k + Direction.RIGHT.vector]!! }
-                .mapKeys { it.key + if (it.key + Vec2(-1, 0) in floor) Vec2(-1, 0) else Vec2(2, 0) }
-
-        val vertical = labels
-                .filterKeys { k -> k + Direction.DOWN.vector in labels }
-                .mapValues { (k, v) -> v.toString() + labels[k + Direction.DOWN.vector]!! }
-                .mapKeys { it.key + if (it.key + Vec2(0, -1) in floor) Vec2(0, -1) else Vec2(0, 2) }
-
-        return horizontal + vertical
-    }
+    private fun labelPortals(floor: Set<Vec2>, labels: Map<Vec2, Char>, direction: Direction): Map<Vec2, String> = labels
+            .filterKeys { k -> k + direction.vector in labels }
+            .mapValues { (k, v) -> v.toString() + labels.getValue(k + direction.vector) }
+            .mapKeys { it.key + if (it.key + direction.reverse.vector in floor) direction.reverse.vector else direction.vector * 2 }
 
     private fun readMaze(input: String): Pair<Set<Vec2>, Map<Vec2, Char>> {
         val floor = mutableSetOf<Vec2>()
@@ -62,10 +58,10 @@ object Day20 : AdventSolution(2019, 20, "Donut maze") {
         input.lines().forEachIndexed { y, line ->
             line.forEachIndexed { x, ch ->
                 when (ch) {
-                    '.'      -> floor += Vec2(x, y)
+                    '.' -> floor += Vec2(x, y)
                     ' ', '#' -> {
                     }
-                    else     -> objectAtLocation[Vec2(x, y)] = ch
+                    else -> objectAtLocation[Vec2(x, y)] = ch
                 }
             }
         }
@@ -76,7 +72,7 @@ object Day20 : AdventSolution(2019, 20, "Donut maze") {
     override fun solvePartTwo(input: String): Int {
         val (floor, labels) = readMaze(input)
 
-        val portals = labelPortals(floor, labels)
+        val portals = labelPortals(floor, labels, Direction.RIGHT) + labelPortals(floor, labels, Direction.DOWN)
 
 
         fun Vec2.isInside() = x == 34 || y == 34 || x == 92 || y == 92
@@ -90,12 +86,13 @@ object Day20 : AdventSolution(2019, 20, "Donut maze") {
                 .toMap()
                 .mapValues { (entrance, exit) -> if (entrance.isInside()) Pair(exit, 1) else Pair(exit, -1) }
 
-        links.forEach (::println)
+        links.forEach(::println)
 
-        fun portalNeighbors(p: Vec2, level: Int) = links[p]?.let { listOf(it.first to it.second + level) }.orEmpty().filter { it.second >=0 }
+        fun portalNeighbors(p: Vec2, level: Int) = links[p]?.let { listOf(it.first to it.second + level) }.orEmpty()
+                .filter { it.second >= 0 }
         val vs = Direction.values().map { it.vector }
         fun floorNeighbors(p: Vec2, level: Int) = vs.map { (it + p) to level }.filter { it.first in floor }
-        fun neighbors(p: Vec2, level: Int) = portalNeighbors(p, level)+floorNeighbors(p, level)
+        fun neighbors(p: Vec2, level: Int) = portalNeighbors(p, level) + floorNeighbors(p, level)
 
 
         val start = portals.asSequence().first { it.value == "AA" }.key to 0
