724811d72182cff789088385d4bba8942a779750
Ruud Wiegers <ruud.wiegers@topicus.nl>
2019-12-22 09:52:24 +0100

    Day 20 split off parsing

--- src/adventofcode/y2019/Day22.kt
+++ src/adventofcode/y2019/Day22.kt
@@ -6,35 +6,42 @@ import adventofcode.util.SimpleParser
 
 fun main() = Day22.solve()
 
-
-typealias DeckAction = (List<Int>) -> List<Int>
-
 object Day22 : AdventSolution(2019, 22, "Magic") {
 
-    override fun solvePartOne(input: String): Any? {
-
-        val p = SimpleParser<DeckAction>()
-                .apply {
-                    rule("deal into new stack") { { it.reversed() } }
-                    rule("deal with increment (\\d+)") { (n) ->
-                        {
-                            val res = IntArray(it.size)
-                            it.forEachIndexed { i, v -> res[i * n.toInt() % it.size] = v }
-                            res.toList()
-                        }
-                    }
-                    rule("cut (\\d+)") { (n) -> { it.drop(n.toInt()) + it.take(n.toInt()) } }
-                    rule("cut -(\\d+)") { (n) -> { it.takeLast(n.toInt()) + it.dropLast(n.toInt()) } }
+    override fun solvePartOne(input: String) = parseInput(input)
+            .fold((0 until 10007).toList()) { deck, action ->
+                when (action) {
+                    is Action.Cut        -> deck.drop(action.n) + deck.take(action.n)
+                    is Action.InverseCut -> deck.takeLast(action.n) + deck.dropLast(action.n)
+                    Action.Reverse       -> deck.reversed()
+                    is Action.Increment  -> IntArray(deck.size).apply {
+                        deck.forEachIndexed { i, v -> this[i * action.n % deck.size] = v }
+                    }.toList()
                 }
+            }
+            .indexOf(2019)
 
-
-        return input.lines()
-                .mapNotNull { p.parse(it) }
-                .fold((0 until 10007).toList()) { deck, action -> action(deck) }
-                .indexOf(2019)
-    }
 
     override fun solvePartTwo(input: String): Any? {
-        return "TODO"
+        return parseInput(input)
     }
+
+    private fun parseInput(input: String): List<Action> {
+        val p = SimpleParser<Action>()
+                .apply {
+                    rule("deal into new stack") { Action.Reverse }
+                    rule("deal with increment (\\d+)") { (n) -> Action.Increment(n.toInt()) }
+                    rule("cut (\\d+)") { (n) -> Action.Cut(n.toInt()) }
+                    rule("cut -(\\d+)") { (n) -> Action.InverseCut(n.toInt()) }
+                }
+
+        return input.lines().mapNotNull { p.parse(it) }
+    }
+}
+
+private sealed class Action {
+    data class Cut(val n: Int) : Action()
+    data class InverseCut(val n: Int) : Action()
+    object Reverse : Action()
+    data class Increment(val n: Int) : Action()
 }
