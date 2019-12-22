8029532f2ab88fd87dfa8c2403d21d16ac131841
Ruud Wiegers <ruud.wiegers@topicus.nl>
2019-12-22 17:07:17 +0100

    Day 22: minor cleanup

--- src/adventofcode/y2019/Day22.kt
+++ src/adventofcode/y2019/Day22.kt
@@ -3,7 +3,6 @@ package adventofcode.y2019
 import adventofcode.AdventSolution
 import adventofcode.solve
 import adventofcode.util.SimpleParser
-import adventofcode.y2019.Day22.ShufflingStep.*
 import java.math.BigInteger
 import java.math.BigInteger.ONE
 import java.math.BigInteger.ZERO
@@ -21,32 +20,32 @@ object Day22 : AdventSolution(2019, 22, "Slam Shuffle") {
             .repeat(101741582076661)
             .evaluateInverse(2020.toBigInteger())
 
+
     private val parser = SimpleParser<ShufflingStep>().apply {
-        rule("deal into new stack") { NewStack }
-        rule("cut (-?\\d+)") { (n) -> Cut(n.toBigInteger()) }
-        rule("deal with increment (\\d+)") { (n) -> Increment(n.toBigInteger()) }
+        rule("deal into new stack") { ShufflingStep.NewStack }
+        rule("cut (-?\\d+)") { (n) -> ShufflingStep.Cut(n.toBigInteger()) }
+        rule("deal with increment (\\d+)") { (n) -> ShufflingStep.Increment(n.toBigInteger()) }
     }
 
-    private fun parseInput(input: String) = input.lineSequence().mapNotNull { parser.parse(it) }
+    private fun parseInput(input: String): Sequence<ShufflingStep> =
+            input.lineSequence().mapNotNull { parser.parse(it) }
+
+    private fun Sequence<ShufflingStep>.composeShufflingRoutine(deckSize: BigInteger): LinearEquation = this
+            .map { it.toLinearEquation(deckSize) }
+            .reduce { acc, exp -> exp.compose(acc) }
+
+    private fun ShufflingStep.toLinearEquation(deckSize: BigInteger) = when (this) {
+        ShufflingStep.NewStack     -> LinearEquation(deckSize - ONE, deckSize - ONE, deckSize)
+        is ShufflingStep.Cut       -> LinearEquation(ONE, deckSize - n, deckSize)
+        is ShufflingStep.Increment -> LinearEquation(n, ZERO, deckSize)
+    }
+
 
     private sealed class ShufflingStep {
         object NewStack : ShufflingStep()
         class Cut(val n: BigInteger) : ShufflingStep()
         class Increment(val n: BigInteger) : ShufflingStep()
     }
-
-    private fun Sequence<ShufflingStep>.composeShufflingRoutine(deckSize: BigInteger) = this
-            .map { it.toLinearEquation(deckSize) }
-            .reduce { acc, exp -> exp.compose(acc) }
-
-
-    private fun ShufflingStep.toLinearEquation(deckSize: BigInteger) = when (this) {
-        NewStack     -> LinearEquation(deckSize - ONE, deckSize - ONE, deckSize)
-        is Cut       -> LinearEquation(ONE, deckSize - n, deckSize)
-        is Increment -> LinearEquation(n, ZERO, deckSize)
-    }
-
-
 
     private data class LinearEquation(val a: BigInteger, val b: BigInteger, val m: BigInteger) {
         fun evaluate(s: BigInteger) = (s * a + b) % m
@@ -55,13 +54,12 @@ object Day22 : AdventSolution(2019, 22, "Slam Shuffle") {
         fun compose(other: LinearEquation) = LinearEquation((a * other.a) % m, (a * other.b + b) % m, m)
 
         fun repeat(exp: Long): LinearEquation {
-            val binaryExpansion = generateSequence(exp) { it / 2 }.takeWhile { it > 0 }.map { it % 2 == 1L }
             val repeatedSquaring = generateSequence(this) { it.compose(it) }
+            val binaryExpansion = generateSequence(exp) { it / 2 }.takeWhile { it > 0 }.map { it % 2 == 1L }
 
             return repeatedSquaring.zip(binaryExpansion) { partial, occurs -> partial.takeIf { occurs } }
                     .filterNotNull()
                     .reduce(LinearEquation::compose)
         }
     }
-
 }
