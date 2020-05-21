1106b1142d8ff25c77372fef197a7ffebe291029
Ruud Wiegers <ruud.wiegers@gmail.com>
2020-05-21 19:15:29 +0200

    Old stuff

--- src/adventofcode/y2015/Day10.kt
+++ src/adventofcode/y2015/Day10.kt
@@ -2,37 +2,26 @@ package adventofcode.y2015
 
 import adventofcode.AdventSolution
 
-
 object Day10 : AdventSolution(2015, 10, "Elves Look, Elves Say") {
 
-	override fun solvePartOne(input: String): String {
-		var res = input
-		repeat(40) { res = looksay(res) }
-		return res.length.toString()
-	}
+    override fun solvePartOne(input: String) = generateSequence(input, this::looksay).drop(40).first().length
 
-	override fun solvePartTwo(input: String): String {
-		var res = input
-		repeat(50) { res = looksay(res) }
-		return res.length.toString()
-	}
+    override fun solvePartTwo(input: String) = generateSequence(input, this::looksay).drop(50).first().length
 
+    private fun looksay(s: String): String {
+        val out = StringBuilder()
+        var count = 1
+        var last = s[0]
 
-	private fun looksay(s: String): String {
-		val out = StringBuilder()
-		var i = 0
-		while (i < s.length) {
-			var count = 1
-			val c = s[i]
-			while (i + 1 < s.length && s[i + 1] == c) {
-				++i
-				++count
-			}
-			out.append(count)
-			out.append(c)
-			i++
-		}
-		return out.toString()
-	}
-
+        for (i in 1 until s.length) {
+            if (s[i] == last)
+                count++
+            else {
+                out.append("$count$last")
+                count = 1
+                last = s[i]
+            }
+        }
+        return out.append("$count$last").toString()
+    }
 }
