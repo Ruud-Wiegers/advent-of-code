cd55be1cd3a02cbf121941f74a21fd83bc62005d
Ruud Wiegers <ruud.wiegers@topicus.nl>
2020-12-06 09:17:59 +0100

    Day 6

---
+++ src/adventofcode/y2020/Day06.kt
@@ -0,0 +1,25 @@
+package adventofcode.y2020
+
+import adventofcode.AdventSolution
+import adventofcode.solve
+
+fun main() = Day06.solve()
+
+object Day06 : AdventSolution(2020, 6, "Custom Customs")
+{
+    override fun solvePartOne(input: String) = solve(input, Set<Char>::union)
+
+    override fun solvePartTwo(input: String) = solve(input, Set<Char>::intersect)
+
+    private fun solve(input: String, reducer: Reducer) = input
+        .splitToSequence("\n\n")
+        .sumBy { g -> solveGroup(g, reducer) }
+
+    private fun solveGroup(group: String, reducer: Reducer) = group
+        .split("\n")
+        .map(String::toSet)
+        .reduce(reducer)
+        .size
+}
+
+private typealias Reducer = (Set<Char>, Set<Char>) -> Set<Char>
