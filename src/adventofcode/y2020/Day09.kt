521ba8d9572c6fbd9bc044b0585a2e8b34987908
Ruud Wiegers <ruud.wiegers@topicus.nl>
2020-12-09 08:58:27 +0100

    Tidying up day 9

--- src/adventofcode/y2020/Day09.kt
+++ src/adventofcode/y2020/Day09.kt
@@ -9,29 +9,36 @@ object Day09 : AdventSolution(2020, 9, "Encoding Error")
 {
     override fun solvePartOne(input: String): Long
     {
+        val windowSize = 25
         val seq = input.lineSequence().map(String::toLong)
-        return seq
-            .windowed(25, partialWindows = false) { it.drop(1).map { r -> r + it[0] } }
-            .windowed(25, partialWindows = false) { it.flatten().toSet() }
-            .zip(seq.drop(25))
-            .first { (valid, n) -> n !in valid }
-            .second
+
+        val validSums = seq
+            .windowed(windowSize) { it.drop(1).map { v -> v + it[0] } }
+            .windowed(windowSize) { it.flatten() }
+
+        return seq.drop(windowSize)
+            .zip(validSums)
+            .first { (n, valid) -> n !in valid }
+            .first
     }
 
-    override fun solvePartTwo(input: String): Long
-    {
-        val target = solvePartOne(input)
-        val seq = input.lines().map(String::toLong)
+    override fun solvePartTwo(input: String) = input.lines()
+        .map(String::toLong)
+        .findSublistSummingTo(solvePartOne(input))
+        .sorted()
+        .let { it.first() + it.last() }
 
-        var total = seq[0]
+    private fun List<Long>.findSublistSummingTo(target: Long): List<Long>
+    {
+        var total = this[0]
         var low = 0
         var high = 0
         while (total != target)
         {
-            if (total < target) total += seq[++high]
-            if (total > target) total -= seq[low++]
+            if (total < target) total += this[++high]
+            if (total > target) total -= this[low++]
         }
 
-        return seq.subList(low, high + 1).sorted().let { it.first() + it.last() }
+        return subList(low, high + 1)
     }
 }
