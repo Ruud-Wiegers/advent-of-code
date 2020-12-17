42be03fe9d7cb734936c1a2c231860ed10190065
Ruud Wiegers <ruud.wiegers@topicus.nl>
2020-12-17 11:18:28 +0100

    fiddling

--- y2020/src/main/kotlin/adventofcode/y2020/Day17.kt
+++ y2020/src/main/kotlin/adventofcode/y2020/Day17.kt
@@ -9,38 +9,30 @@ object Day17 : AdventSolution(2020, 17, "Conway Cubes")
 {
     override fun solvePartOne(input: String): Int
     {
-        val active = buildSet {
-            input.lines().forEachIndexed { y, line ->
-                line.forEachIndexed { x, ch ->
-                    if (ch == '#')
-                        add(listOf(x, y, 0))
-                }
-            }
-        }
+        val active = parse(input).map { it + 0 }.toSet()
 
-        return generateSequence(ConwayCube(active), ConwayCube::next)
-            .take(7).last()
-            .activeCells.size
+        return generateSequence(ConwayCube(active), ConwayCube::next).take(7).last().size
     }
 
     override fun solvePartTwo(input: String): Any
     {
-        val active = buildSet {
-            input.lines().forEachIndexed { y, line ->
-                line.forEachIndexed { x, ch ->
-                    if (ch == '#')
-                        add(listOf(x, y, 0, 0))
-                }
-            }
-        }
+        val active = parse(input).map { it + 0 + 0 }.toSet()
 
-        return generateSequence(ConwayCube(active), ConwayCube::next)
-            .take(7).last()
-            .activeCells.size
+        return generateSequence(ConwayCube(active), ConwayCube::next).take(7).last().size
     }
 
-    private data class ConwayCube(val activeCells: Set<Coordinate>)
+    private fun parse(input:String) = buildList {
+        input.lines().forEachIndexed { y, line ->
+            line.forEachIndexed { x, ch ->
+                if (ch == '#')
+                    add(listOf(x, y))
+            }
+        }
+    }
+
+    private data class ConwayCube(private val activeCells: Set<Coordinate>)
     {
+        val size = activeCells.size
         fun next() = interior().filter(::aliveInNext).toSet().let(::ConwayCube)
 
         private fun aliveInNext(c: Coordinate): Boolean = c.countActiveNeighbors() in if (c in activeCells) 3..4 else 3..3
@@ -61,11 +53,9 @@ object Day17 : AdventSolution(2020, 17, "Conway Cubes")
     }
 }
 
-private fun expand(r: List<IntRange>): List<Coordinate> =
-    if (r.size == 1)
-        r.single().map(::listOf)
-    else
-        r.last().flatMap { v -> expand(r.dropLast(1)).map { it + v } }
-
+private fun expand(ranges: List<IntRange>): List<Coordinate> =
+    ranges.fold(listOf(emptyList())) { acc, r ->
+        acc.flatMap { l -> r.map(l::plus) }
+    }
 
 private typealias Coordinate = List<Int>
