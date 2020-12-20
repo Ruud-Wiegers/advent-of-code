7941b72622fd24501254e46daeff9092a349ada3
Ruud Wiegers <ruud.wiegers@gmail.com>
2020-12-20 17:47:43 +0100

    Day 20, trying to clean up

--- y2020/src/main/kotlin/adventofcode/y2020/Day20.kt
+++ y2020/src/main/kotlin/adventofcode/y2020/Day20.kt
@@ -9,46 +9,35 @@ fun main() = Day20.solve()
 object Day20 : AdventSolution(2020, 20, "Jurassic Jigsaw") {
     override fun solvePartOne(input: String): Any {
         val tiles = parseInput(input)
-        val unmatchedEdges = groupByUnmatchedCount(tiles)
-        return unmatchedEdges[2]!!.reduce(Long::times)
+        val unmatchedEdges = unmatchedEdgeCounts(tiles)
+
+        return unmatchedEdges.filter { it.value == 2 }.keys.reduce(Long::times)
     }
 
     override fun solvePartTwo(input: String): Int {
-        val tiles = parseInput(input)
+        val tiles: List<Tile> = parseInput(input)
 
-        val unmatchedEdges = groupByUnmatchedCount(tiles)
+        val unmatchedEdges = unmatchedEdgeCounts(tiles)
 
-        val corners = unmatchedEdges.getValue(2).toSet()
-        val edges = unmatchedEdges.getValue(1).toSet()
-        require(corners.size == 4)
-        require(edges.size == 40)
+        val tilesByEdgecount = tiles.groupBy { unmatchedEdges[it.id] ?: 0 }.toSortedMap().values.map { it.toSet() }
 
-        val arrangedTiles = arrangePuzzle(tiles.associateBy { it.id }, corners, edges)
+        val arrangedTiles = arrangePuzzle(tilesByEdgecount)
 
         val image = cutTiles(arrangedTiles)
 
-        val allOrientations =
-            generateSequence(Tile(0, image), Tile::rotate).take(4).let { it + it.map(Tile::flip) }.map { it.grid }
-
-        val monster = listOf(
-            "                  # ",
-            "#    ##    ##    ###",
-            " #  #  #  #  #  #   "
+        val monster = Image(
+            listOf(
+                "                  # ",
+                "#    ##    ##    ###",
+                " #  #  #  #  #  #   "
+            )
         )
-        val monsterCount = allOrientations.map { countMonsters(it, monster) }.first { it > 0 }
+        val monsterCount = image.sequenceOfOrientations().map { countMonsters(it, monster) }.first { it > 0 }
 
-        return image.sumOf { it.count { it == '#' } } - monsterCount * monster.sumOf { it.count { it == '#' } }
+        return image.grid.sumOf { it.count { it == '#' } } - monsterCount * monster.grid.sumOf { it.count { it == '#' } }
     }
 
-    private fun countMonsters(image: List<String>, monster: List<String>): Int {
-        fun List<String>.transpose(): List<String> =
-            first().indices.map { index ->
-                asSequence().map { it[index] }.joinToString("")
-            }
-
-
-        val slices =
-            image.windowed(monster.size) { it.transpose().windowed(monster[0].length).map { it.transpose() } }.flatten()
+    private fun countMonsters(image: Image, monster: Image): Int {
 
         fun matches(slice: List<String>, monster: List<String>): Boolean {
             for (y in monster.indices) {
@@ -60,103 +49,109 @@ object Day20 : AdventSolution(2020, 20, "Jurassic Jigsaw") {
             return true
         }
 
-        return slices.count { matches(it, monster) }
+        return image.sequenceOfSlices(monster.grid[0].length, monster.grid.size)
+            .count { matches(it.grid, monster.grid) }
     }
 
 
-    private fun arrangePuzzle(tiles: Map<Long, Tile>, corners: Set<Long>, edges: Set<Long>): List<List<Tile>> {
-        val middleTiles = tiles.keys.filter { it !in corners && it !in edges }.toSet()
-
-        val fullTiles =
-            tiles.mapValues { generateSequence(it.value, Tile::rotate).take(4).let { it + it.map(Tile::flip) } }
-
-
-        fun solve(
-            g: List<List<Tile>>,
-            p: Vec2,
-            unusedCorners: Set<Long>,
-            unusedEdges: Set<Long>,
-            unusedMiddles: Set<Long>
-        ): List<List<Tile>>? {
+    private fun arrangePuzzle(tilesByEdgecount: List<Set<Tile>>): List<List<Tile>> {
+        fun solve(g: List<List<Tile>>, p: Vec2, tilesByEdgecount: List<Set<Tile>>): List<List<Tile>>? {
             fun inc() = if (p.x < 11) p.copy(x = p.x + 1) else p.copy(y = p.y + 1, x = 0)
 
             if (p.y == 12) return g
 
-            fun Int.edge() = this == 0 || this == 11
+            val edges = listOf(p.x == 0, p.x == 11, p.y == 0, p.y == 11).count { it }
 
-            val options = when {
-                p.x.edge() && p.y.edge() -> unusedCorners
-                p.x.edge() || p.y.edge() -> unusedEdges
-                else                     -> unusedMiddles
-            }
 
             fun placeTile(g: List<List<Tile>>, tile: Tile): List<List<Tile>> =
                 if (p.x == 0) g.plusElement(listOf(tile))
                 else g.dropLast(1).plusElement(g.last() + tile)
 
 
-            fun isValidPlacement(tile:Tile):Boolean{
-                if (p.x > 0) {
-                    if (g[p.y][p.x-1].right!= tile.left) return false
-                }
-                if (p.y > 0) {
-                    if (g[p.y-1][p.x].bottom != tile.top) return false
-                }
-                return true
+            fun isValidPlacement(tile: Tile): Boolean = when {
+                p.x > 0 && g[p.y][p.x - 1].right != tile.left -> false
+                p.y > 0 && g[p.y - 1][p.x].bottom != tile.top -> false
+                else                                          -> true
             }
 
-            return options.asSequence()
-                .flatMap { id -> fullTiles.getValue(id).asSequence().map { id to it } }
-                .filter { isValidPlacement(it.second) }
-                .mapNotNull { (id, tile) ->
+            return tilesByEdgecount[edges].asSequence()
+                .flatMap { tile -> tile.img.sequenceOfOrientations() //TODO this is slow, back to explicit preexpansion and lookup
+                    .map { tile.copy(img = it) } }
+                .filter { isValidPlacement(it) }
+                .mapNotNull { tile ->
                     solve(
                         placeTile(g, tile),
                         inc(),
-                        unusedCorners - id,
-                        unusedEdges - id,
-                        unusedMiddles - id
-                    )
+                        tilesByEdgecount.toMutableList().apply { this[edges] = this[edges] - tile })
                 }
                 .firstOrNull()
 
         }
-        return solve(listOf(), Vec2.origin, corners, edges, middleTiles)!!
+        return solve(listOf(), Vec2.origin, tilesByEdgecount)!!
     }
 
-    private fun cutTiles(arrangedTiles: List<List<Tile>>): List<String> = arrangedTiles.flatMap {
-        val map = it.map { it.grid.drop(1).dropLast(1) }
-        map.first().indices.map { index -> map.map { it[index] } }
-            .map { it.map { it.drop(1).dropLast(1) }.joinToString("") }
-    }
+    private fun cutTiles(arrangedTiles: List<List<Tile>>): Image = arrangedTiles
+        .map { line ->
+            line.map { it.img.cropped(1, 1, it.img.width - 2, it.img.height - 2) }
+        }
+        .map { Image.stitchHorizontal(it) }
+        .let { Image.stitchVertical(it) }
 
-
-    private fun groupByUnmatchedCount(tiles: List<Tile>): Map<Int, List<Long>> {
+    private fun unmatchedEdgeCounts(tiles: List<Tile>): Map<Long, Int> {
         val edgeGroups = tiles
             .flatMap { t -> t.allEdges.map { it to t } }
             .groupBy({ it.first }, { it.second })
 
-
-        val umatchedEdges = edgeGroups.values.mapNotNull { it.singleOrNull() }.groupingBy { it.id }.eachCount()
+        return edgeGroups.values.mapNotNull { it.singleOrNull() }.groupingBy { it.id }.eachCount()
             .mapValues { it.value / 2 }
-        return umatchedEdges.entries.groupBy({ it.value }, { it.key })
     }
 
     private fun parseInput(input: String): List<Tile> = input.split("\n\n")
         .map { it.lines() }
-        .map { Tile(it[0].drop(5).dropLast(1).toLong(), it.drop(1)) }
+        .map { Tile(it[0].drop(5).dropLast(1).toLong(), Image(it.drop(1))) }
+}
+
+private data class Tile(val id: Long, val img: Image) {
+    val top = img.grid.first()
+    val right = img.grid.map(String::last).joinToString("")
+    val bottom = img.grid.last()
+    val left = img.grid.map(String::first).joinToString("")
+
+    val allEdges by lazy { listOf(top, right, bottom, left).let { it + it.map(String::reversed) } }
+}
+
+private data class Image(val grid: List<String>) {
+
+    val width = grid[0].length
+    val height = grid.size
+
+    fun flipped() = Image(grid.map(String::reversed))
+
+    fun rotated() = Image(grid.indices.map { y ->
+        grid[0].indices.map { x ->
+            grid[grid.lastIndex - x][y]
+        }.joinToString("")
+    })
+
+    fun cropped(xs: IntRange, ys: IntRange) = Image(grid.slice(ys).map { line -> line.slice(xs) })
+
+    fun cropped(x: Int, y: Int, width: Int, height: Int) = cropped(x until x + width, y until y + height)
 
 
-    data class Tile(val id: Long, val grid: List<String>) {
+    fun sequenceOfOrientations(): Sequence<Image> =
+        generateSequence(this, Image::rotated).take(4).let { it + it.map(Image::flipped) }
 
-        val top = grid.first()
-        val right = grid.map(String::last).joinToString("")
-        val bottom = grid.last()
-        val left = grid.map(String::first).joinToString("")
+    fun sequenceOfSlices(width: Int, height: Int) = sequence {
+        for (y in 0 until grid.size - height)
+            for (x in 0 until grid[0].length - width)
+                yield(cropped(x, y, width, height))
+    }
 
-        fun flip() = copy(grid = grid.map(String::reversed))
-        fun rotate() = copy(grid = grid.indices.map { y -> grid.indices.map { x -> grid[grid.lastIndex-x][y] }.joinToString("") })
+    companion object {
+        fun stitchHorizontal(images: List<Image>) = Image(images.first().grid.indices.map { y ->
+            images.joinToString("") { it.grid[y] }
+        })
 
-        val allEdges by lazy { listOf(top, right, bottom, left).let { it + it.map(String::reversed) } }
-
+        fun stitchVertical(images: List<Image>) = Image(images.flatMap(Image::grid))
     }
 }
