7ae76a9183dfad462c5e46fc912de02f03daa007
Ruud Wiegers <ruud.wiegers@gmail.com>
2020-12-25 13:10:57 +0100

    sneaky sneaky

--- y2020/src/main/kotlin/adventofcode/y2020/Day25.kt
+++ y2020/src/main/kotlin/adventofcode/y2020/Day25.kt
@@ -5,13 +5,12 @@ import adventofcode.solve
 
 fun main() = Day25.solve()
 
-object Day25 : AdventSolution(2020, 25, "Combo Breaker D:<")
-{
-    override fun solvePartOne(input: String): Any
-    {
+object Day25 : AdventSolution(2020, 25, "Combo Breaker D:<") {
+    override fun solvePartOne(input: String): Long {
         val (k0, k1) = input.lines().map(String::toLong)
-        val loop = generateSequence(1L) { it * 7 % 20201227L }.takeWhile { it != k0 }.count()
-        return generateSequence(1L) { it * k1 % 20201227L }.drop(loop).first()
+        return generateSequence(1L) { it * 7L % 20201227L }
+            .takeWhile { it != k0 }
+            .fold(1L) { a, _ -> a * k1 % 20201227L }
     }
 
     override fun solvePartTwo(input: String) = "Free Star!"
