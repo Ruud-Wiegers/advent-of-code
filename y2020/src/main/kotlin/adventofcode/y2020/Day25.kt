13c89763e043e84157cc71eb89f1c3432ba34e9d
Ruud Wiegers <ruud.wiegers@topicus.nl>
2020-12-25 07:21:14 +0100

    Day 25

--- y2020/src/main/kotlin/adventofcode/y2020/Day25.kt
+++ y2020/src/main/kotlin/adventofcode/y2020/Day25.kt
@@ -5,16 +5,14 @@ import adventofcode.solve
 
 fun main() = Day25.solve()
 
-object Day25 : AdventSolution(2020, 24, "Christmas Computer?")
+object Day25 : AdventSolution(2020, 25, "Combo Breaker D:<")
 {
     override fun solvePartOne(input: String): Any
     {
-        return "unsolved"
+        val (k0, k1) = input.lines().map(String::toLong)
+        val loop = generateSequence(1L) { it * 7 % 20201227L }.takeWhile { it != k0 }.count()
+        return generateSequence(1L) { it * k1 % 20201227L }.drop(loop).first()
     }
 
-    override fun solvePartTwo(input: String): Any
-    {
-        return "unsolved"
-    }
-
+    override fun solvePartTwo(input: String) = "Free Star!"
 }
