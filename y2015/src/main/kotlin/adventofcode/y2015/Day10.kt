88844c232035ab63bb5bab9984e53aaf8b3ce31a
Ruud Wiegers <ruud.wiegers@topicus.nl>
2021-09-15 20:07:50 +0200

    speed

--- y2015/src/main/kotlin/adventofcode/y2015/Day10.kt
+++ y2015/src/main/kotlin/adventofcode/y2015/Day10.kt
@@ -2,26 +2,31 @@ package adventofcode.y2015
 
 import adventofcode.AdventSolution
 
-object Day10 : AdventSolution(2015, 10, "Elves Look, Elves Say") {
+object Day10 : AdventSolution(2015, 10, "Elves Look, Elves Say")
+{
 
-    override fun solvePartOne(input: String) = generateSequence(input, this::looksay).drop(40).first().length
+    override fun solvePartOne(input: String) = generateSequence(input, this::looksay).elementAt(40).length
 
-    override fun solvePartTwo(input: String) = generateSequence(input, this::looksay).drop(50).first().length
+    override fun solvePartTwo(input: String) = generateSequence(input, this::looksay).elementAt(50).length
 
-    private fun looksay(s: String): String {
+    private fun looksay(s: String): String
+    {
         val out = StringBuilder()
-        var count = 1
+        var count = 0
         var last = s[0]
 
-        for (i in 1 until s.length) {
-            if (s[i] == last)
+        for (ch in s)
+        {
+            if (ch == last)
                 count++
-            else {
-                out.append("$count$last")
+            else
+            {
+                out.append(count).append(last)
                 count = 1
-                last = s[i]
+                last = ch
             }
         }
-        return out.append("$count$last").toString()
+
+        return out.append(count).append(last).toString()
     }
 }
--- y2015/src/main/kotlin/adventofcode/y2015/Day24.kt
+++ y2015/src/main/kotlin/adventofcode/y2015/Day24.kt
@@ -1,41 +1,47 @@
 package adventofcode.y2015
 
 import adventofcode.AdventSolution
-import java.math.BigInteger
 
 object Day24 : AdventSolution(2015, 24, "It Hangs in the Balance")
 {
+    override fun solvePartOne(input: String) = input.lines().map(String::toInt).let { solve(it, 3) }
 
-    override fun solvePartOne(input: String): String
+    override fun solvePartTwo(input: String) = input.lines().map(String::toInt).let { solve(it, 4) }
+
+    private fun solve(presents: List<Int>, groupCount: Int): Long?
     {
-        val presents = list(input).asSequence()
-        val target = presents.sum() / 3
-        return subsetSum(presents, target)
+        val target = presents.sum() / groupCount
+
+        //meet-in-the-middle style solution
+        val a: Map<Int, Group> = powerSetByWeight(presents.filterIndexed { i, _ -> i % 2 == 0 }, target)
+        val b: Map<Int, Group> = powerSetByWeight(presents.filterIndexed { i, _ -> i % 2 != 0 }, target)
+
+        return (0..target)
+            .minOfOrNull { i ->
+                (a[i] ?: Group.max).mergeWith(b[target - i] ?: Group.max)
+            }?.quantum
     }
 
-    override fun solvePartTwo(input: String): String
+    private fun powerSetByWeight(presents: List<Int>, targetWeight: Int): Map<Int, Group>
     {
-        val presents = list(input).asSequence()
-        val target = presents.sum() / 4
-        return subsetSum(presents, target)
-    }
-
-    private fun subsetSum(presents: Sequence<Int>, target: Int): String
-    {
-        return generateSequence(presents.map { listOf(it) }) { prev ->
-            prev.flatMap { subset ->
-                val t = target - subset.sum()
-                presents.dropWhile { it <= subset.last() }
-                    .takeWhile { it <= t }
-                    .map { subset + it }
-            }
+        return presents.fold(listOf(Group.empty)) { old, present ->
+            old + old.map { it.addPresent(present) }.filter { it.weight <= targetWeight }
         }
-            .map { s -> s.filter { it.sum() == target } }
-            .first { it.any() }
-            .map { it.fold(BigInteger.ONE) { x, y -> x * y.toBigInteger() } }
-            .minOrNull()
-            .toString()
+            .groupingBy { it.weight }
+            .reduce { _, min, candidate -> minOf(min, candidate) }
     }
 
-    private fun list(input: String): List<Int> = input.lines().map { it.toInt() }
+    private data class Group(val count: Int, val weight: Int, val quantum: Long) : Comparable<Group>
+    {
+        fun addPresent(o: Int) = Group(count + 1, weight + o, quantum * o)
+        fun mergeWith(o: Group) = Group(count + o.count, weight + o.weight, quantum * o.quantum)
+
+        companion object
+        {
+            val empty = Group(0, 0, 1L)
+            val max = Group(1000, 1000000, 100000000L)
+        }
+
+        override fun compareTo(other: Group) = compareValuesBy(this, other, Group::weight, Group::count, Group::quantum)
+    }
 }
