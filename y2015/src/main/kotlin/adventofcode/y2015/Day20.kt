7f71906ce16772c6437e6679e3e62048f5c5b9a3
Ruud Wiegers <ruud.wiegers@topicus.nl>
2021-09-15 16:38:08 +0200

    tiny fix

--- y2015/src/main/kotlin/adventofcode/y2015/Day20.kt
+++ y2015/src/main/kotlin/adventofcode/y2015/Day20.kt
@@ -1,30 +1,36 @@
 package adventofcode.y2015
 
 import adventofcode.AdventSolution
-import adventofcode.solve
 
-object Day20 : AdventSolution(2015, 20, "Infinite Elves and Infinite Houses") {
+object Day20 : AdventSolution(2015, 20, "Infinite Elves and Infinite Houses")
+{
 
-    override fun solvePartOne(input: String): String {
+    override fun solvePartOne(input: String): Int
+    {
         val sigma = input.toInt() / 10
-        val arr = IntArray(sigma / 4) { 1 }
-        for (i in 2 until arr.size) {
-            for (j in i until arr.size step i) {
-                arr[j] += i
+        val arr = IntArray(sigma / 2) { 1 }
+        for (elf in 2 until arr.size)
+        {
+            for (house in elf until arr.size step elf)
+            {
+                arr[house] += elf
             }
         }
-        return arr.indexOfFirst { it >= sigma }.toString()
+        return arr.indexOfFirst { it > sigma }
     }
 
-    override fun solvePartTwo(input: String): String {
-        val target = input.toInt()
-        val arr = IntArray(target / 11)
-        for (elf in 1 until target) {
-            for (it in elf..(50 * elf).coerceAtMost(arr.lastIndex) step elf) {
-                arr[it] += elf * 11
+    override fun solvePartTwo(input: String): Int
+    {
+        val target = input.toInt() / 11
+        val arr = IntArray(target / 2)
+        for (visit in 1..50)
+        {
+            for (elf in 1 until arr.size / visit)
+            {
+                arr[elf * visit] += elf
             }
         }
-        return arr.indexOfFirst { it >= target }.toString()
+        return arr.indexOfFirst { it > target }
     }
 }
 
--- y2017/src/main/kotlin/adventofcode/y2017/Day20.kt
+++ y2017/src/main/kotlin/adventofcode/y2017/Day20.kt
@@ -1,54 +1,64 @@
 package adventofcode.y2017
 
 import adventofcode.AdventSolution
+import adventofcode.solve
 import kotlin.math.abs
 
-object Day20 : AdventSolution(2017, 20, "Particle Swarm") {
-
-	//this comparator is not correct, but works for my input :S
-	override fun solvePartOne(input: String): String {
-		val particles: List<Particle> = parse(input)
-		val comparator = compareBy<Particle>({ it.acceleration.magnitude }, { it.velocity.magnitude }, { it.position.magnitude })
-		val nearestParticle = particles.minWith(comparator)
-		return particles.indexOf(nearestParticle).toString()
-	}
-
-	override fun solvePartTwo(input: String): String {
-		var particles: List<Particle> = parse(input)
-
-		//1000 is just a guess
-		repeat(1000) {
-			particles = particles.groupBy { it.position }
-					.values
-					.filter { it.size == 1 }
-					.map { it[0].tick() }
-		}
-		return particles.size.toString()
-	}
-
+fun main()
+{
+    Day20.solve()
 }
 
-private fun parse(input: String): List<Particle> {
-	return input.splitToSequence(',', '\n')
-			.map { it.filter { it in "-0123456789" }.toInt() }
-			.chunked(3) { Vector(it[0], it[1], it[2]) }
-			.chunked(3) { Particle(it[0], it[1], it[2]) }
-			.toList()
+object Day20 : AdventSolution(2017, 20, "Particle Swarm")
+{
+
+    //this comparator is not correct, but works for my input :S
+    override fun solvePartOne(input: String): Int
+    {
+        val particles: List<Particle> = parse(input)
+        val nearestParticle = particles.minByOrNull { it.acceleration.magnitude }
+        return particles.indexOf(nearestParticle)
+    }
+
+    override fun solvePartTwo(input: String): Int
+    {
+        var particles: List<Particle> = parse(input)
+
+        //1000 is just a guess
+        repeat(1000) {
+            particles = particles.groupBy(Particle::position)
+                .values
+                .mapNotNull(List<Particle>::singleOrNull)
+                .map(Particle::tick)
+        }
+        return particles.size
+    }
+}
+
+private fun parse(input: String): List<Particle>
+{
+    return input.splitToSequence(',', '\n')
+        .map { it.filter { it in "-0123456789" }.toInt() }
+        .chunked(3) { (x, y, z) -> Vector(x, y, z) }
+        .chunked(3) { (p, v, a) -> Particle(p, v, a) }
+        .toList()
 }
 
 data class Particle(
-		val position: Vector,
-		val velocity: Vector,
-		val acceleration: Vector
-) {
-	fun tick(): Particle = Particle(
-			position + velocity + acceleration,
-			velocity + acceleration,
-			acceleration
-	)
+    val position: Vector,
+    val velocity: Vector,
+    val acceleration: Vector
+)
+{
+    fun tick(): Particle = Particle(
+        position + velocity + acceleration,
+        velocity + acceleration,
+        acceleration
+    )
 }
 
-data class Vector(val x: Int, val y: Int, val z: Int) {
-	operator fun plus(v: Vector) = Vector(x + v.x, y + v.y, z + v.z)
-	val magnitude get() = sequenceOf(x, y, z).map(::abs).sum()
+data class Vector(val x: Int, val y: Int, val z: Int)
+{
+    operator fun plus(v: Vector) = Vector(x + v.x, y + v.y, z + v.z)
+    val magnitude get() = sequenceOf(x, y, z).map(::abs).sum()
 }
