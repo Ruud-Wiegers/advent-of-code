81dfee5e355b838c81431e6aa9bf86bcb8720e82
Ruud Wiegers <ruud.wiegers@topicus.nl>
2021-12-01 06:36:51 +0100

    Day 1

--- y2021/src/main/kotlin/adventofcode/y2021/Day01.kt
+++ y2021/src/main/kotlin/adventofcode/y2021/Day01.kt
@@ -1,42 +1,19 @@
 package adventofcode.y2021
 
 import adventofcode.AdventSolution
-import adventofcode.solve
 
-fun main() = Day01.solve()
-
-object Day01 : AdventSolution(2021, 1, "Report Repair")
+object Day01 : AdventSolution(2021, 1, "Sonar Sweep")
 {
+    override fun solvePartOne(input: String) = input
+        .lineSequence()
+        .map(String::toInt)
+        .zipWithNext { a, b -> a < b }
+        .count { it }
 
-    override fun solvePartOne(input: String): Int
-    {
-
-        val entries = input
-            .lines()
-            .map(String::toInt)
-            .filter { it < 2020 }
-            .toSortedSet()
-
-        return entries
-            .first { 2020 - it in entries }
-            .let { (2020 - it) * it }
-    }
-
-    override fun solvePartTwo(input: String): Any
-    {
-        val entries = input.lines().map(String::toInt).filter { it < 2020 }.toSortedSet()
-
-        for (a in entries.subSet(2020/3,2020))
-        {
-            for (b in entries.subSet((2020-a)/2,a+1))
-            {
-                val c = 2020 - a - b
-                if (c in entries)
-                {
-                    return a * b * c
-                }
-            }
-        }
-        return "No solution"
-    }
+    override fun solvePartTwo(input: String) = input
+        .lineSequence()
+        .map(String::toInt)
+        .windowed(3) { it.sum() }
+        .zipWithNext { a, b -> a < b }
+        .count { it }
 }
