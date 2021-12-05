b5423986eb96b0d8ead202fed645665d08482889
Ruud Wiegers <ruud.wiegers@topicus.nl>
2021-12-05 08:30:00 +0100

    Day 5: Hydrothermal Venture
    conciseness > performance, right?

--- y2021/src/main/kotlin/adventofcode/y2021/Day05.kt
+++ y2021/src/main/kotlin/adventofcode/y2021/Day05.kt
@@ -31,23 +31,21 @@ object Day05 : AdventSolution(2021, 5, "Hydrothermal Venture")
     override fun solvePartTwo(input: String) = parseInput(input)
         .flatMap { (a, b) ->
             val delta = (b - a)
-            val sign = Vec2(delta.x.sign, delta.y.sign)
+            val step = Vec2(delta.x.sign, delta.y.sign)
             val length = max(delta.x.absoluteValue, delta.y.absoluteValue) + 1
-            generateSequence(a) { it + sign }.take(length)
+            generateSequence(a, step::plus).take(length)
         }
         .groupingBy { it }
         .eachCount()
         .count { it.value > 1 }
 
-    private fun parseInput(input: String): Sequence<Area>
+    private fun parseInput(input: String): Sequence<Pair<Vec2, Vec2>>
     {
         val regex = """(\d+),(\d+) -> (\d+),(\d+)""".toRegex()
         return input.lineSequence()
             .map { regex.matchEntire(it)!!.destructured }
             .map { (x1, y1, x2, y2) ->
-                Area(Vec2(x1.toInt(), y1.toInt()), Vec2(x2.toInt(), y2.toInt()))
+                Pair(Vec2(x1.toInt(), y1.toInt()), Vec2(x2.toInt(), y2.toInt()))
             }
     }
-
-    private data class Area(val a: Vec2, val b: Vec2)
 }
