f3cf1ce2d68bf9d15269e64cafae866598621ed5
Ruud Wiegers <ruud.wiegers@topicus.nl>
2021-12-08 11:18:22 +0100

    Day 8 cleanup

--- y2021/src/main/kotlin/adventofcode/y2021/Day08.kt
+++ y2021/src/main/kotlin/adventofcode/y2021/Day08.kt
@@ -27,28 +27,26 @@ object Day08 : AdventSolution(2021, 8, "???")
                 message.split(" ").map(String::toSet)
             )
         }
+
+    private fun List<Set<Char>>.digitPatterns(): List<Set<Char>>
+    {
+        val one = first { it.size == 2 }
+        val four = first { it.size == 4 }
+        val seven = first { it.size == 3 }
+        val eight = first { it.size == 7 }
+
+        val bottomLeftSegment = ('a'..'g').first { segment -> count { segment in it } == 4 }
+
+        val two = first { it.size == 5 && bottomLeftSegment in it }
+        val three = first { it.size == 5 && (it intersect one).size == 2 }
+        val five = first { it.size == 5 && it != two && it != three }
+
+        val six = first { it.size == 6 && (it intersect one).size == 1 }
+        val nine = first { it.size == 6 && bottomLeftSegment !in it }
+        val zero = first { it.size == 6 && it != six && it != nine }
+
+        return listOf(zero, one, two, three, four, five, six, seven, eight, nine)
+    }
+
+    private data class DecodingProblem(val digits: List<Set<Char>>, val message: List<Set<Char>>)
 }
-
-private fun List<Segments>.digitPatterns(): List<Segments>
-{
-    val one = first { it.size == 2 }
-    val four = first { it.size == 4 }
-    val seven = first { it.size == 3 }
-    val eight = first { it.size == 7 }
-
-    val bottomLeftSegment = ('a'..'g').first { segment -> count { segment in it } == 4 }
-
-    val two = first { it.size == 5 && bottomLeftSegment in it }
-    val three = first { it.size == 5 && (it intersect one).size == 2 }
-    val five = first { it.size == 5 && it != two && it != three }
-
-    val six = first { it.size == 6 && (it intersect one).size == 1 }
-    val nine = first { it.size == 6 && bottomLeftSegment !in it }
-    val zero = first { it.size == 6 && it != six && it != nine }
-
-    return listOf(zero, one, two, three, four, five, six, seven, eight, nine)
-}
-
-private data class DecodingProblem(val digits: List<Segments>, val message: List<Segments>)
-
-private typealias Segments = Set<Char>
