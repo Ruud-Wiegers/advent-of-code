2140fb3a517da0f24cfe46d7f39f745fffacf19c
Ruud Wiegers <ruud.wiegers@topicus.nl>
2021-12-09 10:14:06 +0100

    Day[9]]

--- y2021/src/main/kotlin/adventofcode/y2021/Day09.kt
+++ y2021/src/main/kotlin/adventofcode/y2021/Day09.kt
@@ -5,60 +5,51 @@ import adventofcode.solve
 import adventofcode.util.vector.Vec2
 import adventofcode.util.vector.neighbors
 
-fun main() {
+fun main()
+{
     Day09.solve()
 }
 
-object Day09 : AdventSolution(2021, 9, "Smoke Basin") {
-    override fun solvePartOne(input: String): Int {
+object Day09 : AdventSolution(2021, 9, "Smoke Basin")
+{
+    override fun solvePartOne(input: String): Int
+    {
         val grid = Grid(parseInput(input))
-
-        return grid.grid.indices.flatMap { y ->
-            grid.grid[0].indices.map { x ->
-                Vec2(x, y)
-            }
-        }.filter { it.neighbors().all { n -> (grid.get(n) ?: 10) > grid.get(it)!! } }
-            .sumOf { grid.get(it)!! + 1 }
-
-
+        return grid.covering()
+            .filter { it.neighbors().all { n -> (grid[n]) > grid[it] } }
+            .sumOf { grid[it] + 1 }
     }
 
-    override fun solvePartTwo(input: String): Int {
+    override fun solvePartTwo(input: String): Int
+    {
         val grid = Grid(parseInput(input))
 
-        val starts = grid.grid.indices.flatMap { y ->
-            grid.grid[0].indices.map { x ->
-                Vec2(x, y)
-            }
-        }.filter { it.neighbors().all { n -> (grid.get(n) ?: 10) > grid.get(it)!! } }
+        val lowPoints = grid.covering().filter { it.neighbors().all { n -> (grid[n]) > grid[it] } }
 
-        val map = starts.map { bfs(grid, it) }
-        return map.sorted().takeLast(3).reduce(Int::times)
-    }
-
-
-    private data class Grid(val grid: List<List<Int>>) {
-        fun get(v: Vec2) = grid.getOrNull(v.y)?.getOrNull(v.x)
-        fun neighbors(v: Vec2) = v.neighbors().mapNotNull { get(it) }
-
+        return lowPoints.map { search(grid, it) }.sorted().takeLast(3).reduce(Int::times)
     }
 
     private fun parseInput(input: String) = input.lines().map { it.toList().map { it - '0' } }
 
+    private data class Grid(val grid: List<List<Int>>)
+    {
+        operator fun get(v: Vec2) = grid.getOrNull(v.y)?.getOrNull(v.x) ?: 10
 
-    private fun bfs(grid: Grid, start: Vec2): Int {
+        fun covering() = grid.indices.flatMap { y ->
+            grid[0].indices.map { x -> Vec2(x, y) }
+        }
+    }
 
+    private fun search(grid: Grid, start: Vec2): Int
+    {
         val open = mutableListOf(start)
         val closed = mutableSetOf<Vec2>()
-        while (open.isNotEmpty()) {
-
+        while (open.isNotEmpty())
+        {
             val candidate = open.removeLast()
             closed += candidate
-            open += candidate.neighbors().filter { it !in closed }
-                .filter { grid.get(it) in grid.get(candidate)!! until 9 }
-
+            open += candidate.neighbors().filter { it !in closed }.filter { grid[it] in grid[candidate] until 9 }
         }
         return closed.size
     }
-
 }
