22fb522c941b155f6346489835fecb6268ac6f59
Ruud Wiegers <ruud.wiegers@topicus.nl>
2021-12-09 13:00:58 +0100

    Day[9]

--- y2021/src/main/kotlin/adventofcode/y2021/Day09.kt
+++ y2021/src/main/kotlin/adventofcode/y2021/Day09.kt
@@ -5,51 +5,42 @@ import adventofcode.solve
 import adventofcode.util.vector.Vec2
 import adventofcode.util.vector.neighbors
 
-fun main()
-{
-    Day09.solve()
-}
-
 object Day09 : AdventSolution(2021, 9, "Smoke Basin")
 {
     override fun solvePartOne(input: String): Int
     {
-        val grid = Grid(parseInput(input))
-        return grid.covering()
-            .filter { it.neighbors().all { n -> (grid[n]) > grid[it] } }
-            .sumOf { grid[it] + 1 }
+        val grid = parseInput(input)
+        val lowPoints = grid.covering().filter { it.neighbors().all { n -> grid[n] > grid[it] } }
+        return lowPoints.sumOf { grid[it] + 1 }
     }
 
     override fun solvePartTwo(input: String): Int
     {
-        val grid = Grid(parseInput(input))
-
-        val lowPoints = grid.covering().filter { it.neighbors().all { n -> (grid[n]) > grid[it] } }
-
-        return lowPoints.map { search(grid, it) }.sorted().takeLast(3).reduce(Int::times)
+        val grid = parseInput(input)
+        val lowPoints = grid.covering().filter { it.neighbors().all { n -> grid[n] > grid[it] } }
+        fun next(source: Vec2) = source.neighbors().filter { n -> grid[n] in grid[source] until grid.maxHeight }
+        return lowPoints.map { start -> findReachable(start, ::next).size }.sorted().takeLast(3).reduce(Int::times)
     }
 
-    private fun parseInput(input: String) = input.lines().map { it.toList().map { it - '0' } }
+    private fun parseInput(input: String) = input.lines().map { it.toList().map { it - '0' } }.let(::Grid)
 
-    private data class Grid(val grid: List<List<Int>>)
+    private data class Grid(private val grid: List<List<Int>>)
     {
-        operator fun get(v: Vec2) = grid.getOrNull(v.y)?.getOrNull(v.x) ?: 10
-
-        fun covering() = grid.indices.flatMap { y ->
-            grid[0].indices.map { x -> Vec2(x, y) }
-        }
+        val maxHeight = 9
+        operator fun get(v: Vec2) = grid.getOrNull(v.y)?.getOrNull(v.x) ?: maxHeight
+        fun covering() = grid.indices.flatMap { y -> grid[0].indices.map { x -> Vec2(x, y) } }
     }
+}
 
-    private fun search(grid: Grid, start: Vec2): Int
+private fun <T> findReachable(start: T, next: (source: T) -> Iterable<T>): Set<T>
+{
+    val open = mutableListOf(start)
+    val closed = mutableSetOf<T>()
+    while (open.isNotEmpty())
     {
-        val open = mutableListOf(start)
-        val closed = mutableSetOf<Vec2>()
-        while (open.isNotEmpty())
-        {
-            val candidate = open.removeLast()
-            closed += candidate
-            open += candidate.neighbors().filter { it !in closed }.filter { grid[it] in grid[candidate] until 9 }
-        }
-        return closed.size
+        val candidate = open.removeLast()
+        closed += candidate
+        open += next(candidate).filter { it !in closed }
     }
+    return closed
 }
--- y2021/src/main/kotlin/adventofcode/y2021/runAll.kt
+++ y2021/src/main/kotlin/adventofcode/y2021/runAll.kt
@@ -8,7 +8,8 @@ import kotlin.system.measureTimeMillis
 fun main() {
     measureTimeMillis {
         listOf(
-            Day01, Day02, Day03, Day04, Day05, Day06, Day07, Day08
+            Day01, Day02, Day03, Day04, Day05,
+            Day06, Day07, Day08, Day09
         )
             .forEach(AdventSolution::solve)
     }.let { println(formattedTime(it)) }
