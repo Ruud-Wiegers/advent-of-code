c07f404385932c981e81ee9f8108c88e06ec245f
Ruud Wiegers <ruud.wiegers@gmail.com>
2021-12-11 12:14:07 +0100

    An immutable Day 11

--- y2021/src/main/kotlin/adventofcode/y2021/Day11.kt
+++ y2021/src/main/kotlin/adventofcode/y2021/Day11.kt
@@ -10,43 +10,35 @@ fun main() {
 
 object Day11 : AdventSolution(2021, 11, "Dumbo Octopus") {
     override fun solvePartOne(input: String) =
-        generateSequence(parseInput(input), MutableGrid::next).drop(1).take(100).sumOf(MutableGrid::flashed)
+        generateSequence(parseInput(input), Grid::next).drop(1).take(100).sumOf(Grid::flashed)
 
     override fun solvePartTwo(input: String) =
-        generateSequence(parseInput(input), MutableGrid::next).map(MutableGrid::flashed).indexOfFirst(100::equals)
-
+        generateSequence(parseInput(input), Grid::next).map(Grid::flashed).indexOfFirst(100::equals)
 
     private fun parseInput(input: String) = input.lines()
-        .map { line -> line.map { Character.getNumericValue(it) }.toMutableList() }
-        .let(::MutableGrid)
-
-    private data class MutableGrid(val grid: List<MutableList<Int>>) {
-        fun next(): MutableGrid {
-            val g = MutableGrid(grid = grid.map { it.map { it + 1 }.toMutableList() })
-            val open = g.covering().filter { g[it] > 9 }.toMutableList()
-            val closed = mutableSetOf<Vec2>()
-            while (open.isNotEmpty()) {
-                val candidate = open.removeLast()
-                if (candidate in closed) continue
-                closed += candidate
-                val n = neighbors(candidate)
-                n.forEach { v -> g[v]++ }
-                open += n.filter { g[it] > 9 }
-            }
-            closed.forEach { g[it] = 0 }
-            return g
+        .flatMapIndexed { y, line ->
+            line.mapIndexed { x, ch -> Vec2(x, y) to Character.getNumericValue(ch) }
         }
+        .toMap()
+        .let(::Grid)
 
-        operator fun get(v: Vec2) = grid[v.y][v.x]
-        operator fun set(v: Vec2, new: Int) {
-            grid[v.y][v.x] = new
-        }
+    private data class Grid(val cells: Map<Vec2, Int>) {
 
-        fun covering() = grid.indices.flatMap { y -> grid[0].indices.map { x -> Vec2(x, y) } }
-        fun neighbors(v: Vec2) =
-            (-1..1).flatMap { y -> (-1..1).map { x -> Vec2(x, y) } }.map { it + v }
-                .filter { (x, y) -> y in grid.indices && x in grid[0].indices }
+        fun flashed(): Int = cells.count { it.value == 0 }
 
-        fun flashed(): Int = grid.flatten().count { it == 0 }
+        fun next(): Grid = generateSequence(grow(), Grid::flash)
+            .zipWithNext().first { it.first == it.second }.first
+
+        private fun grow() = Grid(cells = cells.mapValues { it.value + 1 })
+
+        private fun flash() = Grid(cells = cells.mapValues { (k, v) ->
+            if (v in 1..9) v + neighbors(k).count { cells.getValue(it) > 9 }
+            else 0
+        })
+
+        private fun neighbors(v: Vec2) =
+            (-1..1).flatMap { y -> (-1..1).map { x -> Vec2(x, y) } }
+                .map { it + v }
+                .filter { it in cells.keys }
     }
 }
