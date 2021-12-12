066390320be2d4d24dbbb90f892a1eedc6c337d1
Ruud Wiegers <ruud.wiegers@gmail.com>
2021-12-12 14:59:59 +0100

    Day 12 with slightly less overkill

--- y2021/src/main/kotlin/adventofcode/y2021/Day12.kt
+++ y2021/src/main/kotlin/adventofcode/y2021/Day12.kt
@@ -7,9 +7,9 @@ object Day12 : AdventSolution(2021, 12, "Passage Pathing") {
 
     override fun solvePartTwo(input: String) = solve(input, Path::visit2)
 
-    private inline fun solve(input: String, visit: Path.(cave: Cave) -> Path?): Int {
-        val graph = parseInput(input)
-        var open = mapOf(Path.initial to 1)
+    private inline fun solve(input: String, visit: Path.(cave: String) -> Path?): Int {
+        val graph = parseToGraph(input)
+        var open = mapOf(Path("start", emptySet(), false) to 1)
         var complete = 0
         while (open.isNotEmpty()) {
             open = open.flatMap { (path, count) ->
@@ -17,48 +17,30 @@ object Day12 : AdventSolution(2021, 12, "Passage Pathing") {
             }
                 .groupingBy { it.first }
                 .fold(0) { sum, (_, count) -> sum + count }
-            complete += open.asSequence().filter { it.key.current == Cave.End }.sumOf { it.value }
+            complete += open.asSequence().filter { it.key.current == "end"}.sumOf { it.value }
         }
         return complete
     }
 
-    private data class Path(val current: Cave, val history: Set<Cave.Small>, val doubled: Boolean) {
-        fun visit1(cave: Cave) = when (cave) {
-            !is Cave.Small -> copy(current = cave)
-            !in history -> copy(current = cave, history = history + cave)
+    private fun parseToGraph(input: String): Map<String, List<String>> =
+        input.lineSequence().map { it.substringBefore('-') to it.substringAfter('-') }
+            .flatMap { (a, b) -> listOf(a to b, b to a) }
+            .filter { it.second !="start" }
+            .filter { it.first != "end"}
+            .groupBy({ it.first }, { it.second })
+
+    private data class Path(val current: String, val history: Set<String>, val doubled: Boolean) {
+        fun visit1(cave: String) = when {
+            cave.lowercase() != cave -> copy(current = cave)
+            cave !in history -> copy(current = cave, history = history + cave)
             else -> null
         }
 
-        fun visit2(cave: Cave) = when {
-            cave !is Cave.Small -> copy(current = cave)
+        fun visit2(cave: String) = when {
+            cave.lowercase() != cave -> copy(current = cave)
             cave !in history -> copy(current = cave, history = history + cave)
             !doubled -> copy(current = cave, doubled = true)
             else -> null
         }
-
-        companion object {
-            val initial = Path(Cave.Start, emptySet(), false)
-        }
-    }
-
-    private fun parseInput(input: String) =
-        input.lineSequence().map { it.substringBefore('-').toCave() to it.substringAfter('-').toCave() }
-            .flatMap { (a, b) -> listOf(a to b, b to a) }
-            .filter { it.second != Cave.Start }
-            .filter { it.first != Cave.End }
-            .groupBy({ it.first }, { it.second })
-
-    private fun String.toCave() = when (this) {
-        "start" -> Cave.Start
-        "end" -> Cave.End
-        lowercase() -> Cave.Small(this)
-        else -> Cave.Large(this)
-    }
-
-    private sealed class Cave {
-        object Start : Cave()
-        object End : Cave()
-        data class Large(val text: String) : Cave()
-        data class Small(val text: String) : Cave()
     }
 }
