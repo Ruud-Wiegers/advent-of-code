6c2dd5acb1f36ff4ef60a9e4d8ab3fec2f5b0036
Ruud Wiegers <ruud.wiegers@topicus.nl>
2021-12-16 09:55:40 +0100

    Day 16 Cleanup

--- y2021/src/main/kotlin/adventofcode/y2021/Day16.kt
+++ y2021/src/main/kotlin/adventofcode/y2021/Day16.kt
@@ -1,94 +1,75 @@
 package adventofcode.y2021
 
 import adventofcode.AdventSolution
-import adventofcode.solve
 
-fun main() {
-    Day16.solve()
+object Day16 : AdventSolution(2021, 16, "Packet Decoder") {
+    override fun solvePartOne(input: String): Long = PacketDecoder(input).parsePacket().let(::sumOfVersions)
+    override fun solvePartTwo(input: String): Long = PacketDecoder(input).parsePacket().let(::eval)
 }
 
-object Day16 : AdventSolution(2021, 16, "BITS") {
-    override fun solvePartOne(input: String): Long {
-        val iter = BitsReader(input)
-        val p = parsePacket(iter)
+private sealed class Packet(val version: Int, val type: Int)
+private class Literal(version: Int, type: Int, val data: Long) : Packet(version, type)
+private class Operator(version: Int, type: Int, val data: List<Packet>) : Packet(version, type)
 
-        fun versions(p: Packet): Long = when (p) {
-            is Literal -> p.version.toLong()
-            is Operator -> p.version + p.data.sumOf(::versions)
-        }
+private class PacketDecoder(input: String) {
+    private val binary: String = input.toBigInteger(16).toString(2)
+    private var mark = 0
 
-        return versions(p)
+    private fun readBits(i: Int): Int {
+        val result = binary.substring(mark, mark + i).toInt(2)
+        mark += i
+        return result
     }
 
-    override fun solvePartTwo(input: String): Long {
-        val iter = BitsReader(input)
-        val p = parsePacket(iter)
-
-        fun eval(p: Packet): Long = when (p) {
-            is Literal -> p.data
-            is Operator -> when (p.type) {
-                0 -> p.data.sumOf(::eval)
-                1 -> p.data.map(::eval).reduce(Long::times)
-                2 -> p.data.minOf(::eval)
-                3 -> p.data.maxOf(::eval)
-                5 -> p.data.map(::eval).let { if (it[0] > it[1]) 1L else 0L }
-                6 -> p.data.map(::eval).let { if (it[0] < it[1]) 1L else 0L }
-                7 -> p.data.map(::eval).let { if (it[0] == it[1]) 1L else 0L }
-                else -> throw IllegalStateException()
-            }
-        }
-
-        return eval(p)
-    }
-
-    private class BitsReader(input: String) {
-        val binary: String = input.toBigInteger(16).toString(2)
-        var mark = 0
-
-        fun readBits(i: Int): Int {
-            val result = binary.substring(mark, mark + i).toInt(2)
-            mark += i
-            return result
-        }
-
-    }
-
-    private sealed class Packet(val version: Int, val type: Int)
-    private class Literal(version: Int, type: Int, val data: Long) : Packet(version, type)
-    private class Operator(version: Int, type: Int, val data: List<Packet>) : Packet(version, type)
-
-    private fun parsePacket(reader: BitsReader): Packet {
-        val version = reader.readBits(3)
-        val type = reader.readBits(3)
+    fun parsePacket(): Packet {
+        val version = readBits(3)
+        val type = readBits(3)
         return when (type) {
-            4 -> parseLiteral(reader, version, type)
-            else -> parseOperator(reader, version, type)
+            4    -> Literal(version, type, parseLiteral())
+            else -> Operator(version, type, parseSubpackets())
         }
     }
 
-    private fun parseLiteral(reader: BitsReader, version: Int, type: Int): Packet {
+    private fun parseLiteral(): Long {
         var result = 0L
         do {
-            val next = reader.readBits(1) == 1
+            val next = readBits(1) == 1
             result *= 16
-            result += reader.readBits(4)
+            result += readBits(4)
         } while (next)
-        return Literal(version, type, result)
+        return result
     }
 
-    private fun parseOperator(reader: BitsReader, version: Int, type: Int): Packet {
-        val lengthType = reader.readBits(1)
-        val subPackets = if (lengthType == 0) {
-            val lastBit = reader.readBits(15) + reader.mark
+    private fun parseSubpackets(): List<Packet> {
+        return if (readBits(1) == 0) {
+            val lastBit = readBits(15) + mark
             buildList {
-                while (reader.mark < lastBit) {
-                    add(parsePacket(reader))
+                while (mark < lastBit) {
+                    add(parsePacket())
                 }
             }
         } else {
-            val packetCount = reader.readBits(11)
-            List(packetCount) { parsePacket(reader) }
+            val packetCount = readBits(11)
+            List(packetCount) { parsePacket() }
         }
-        return Operator(version, type, subPackets)
+    }
+}
+
+private fun sumOfVersions(p: Packet): Long = when (p) {
+    is Literal  -> p.version.toLong()
+    is Operator -> p.version + p.data.sumOf(::sumOfVersions)
+}
+
+private fun eval(p: Packet): Long = when (p) {
+    is Literal  -> p.data
+    is Operator -> when (p.type) {
+        0    -> p.data.sumOf(::eval)
+        1    -> p.data.map(::eval).reduce(Long::times)
+        2    -> p.data.minOf(::eval)
+        3    -> p.data.maxOf(::eval)
+        5    -> p.data.map(::eval).let { if (it[0] > it[1]) 1L else 0L }
+        6    -> p.data.map(::eval).let { if (it[0] < it[1]) 1L else 0L }
+        7    -> p.data.map(::eval).let { if (it[0] == it[1]) 1L else 0L }
+        else -> throw IllegalStateException()
     }
 }
