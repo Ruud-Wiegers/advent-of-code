70388fdb9e8e44450d51c898f9a987432d0d419c
Ruud Wiegers <ruud.wiegers@gmail.com>
2021-12-21 22:18:42 +0100

    Day 21... memoized solution is nicer

--- y2021/src/main/kotlin/adventofcode/y2021/Day21.kt
+++ y2021/src/main/kotlin/adventofcode/y2021/Day21.kt
@@ -2,7 +2,7 @@ package adventofcode.y2021
 
 import adventofcode.AdventSolution
 
-object Day21 : AdventSolution(2021, 21, "???") {
+object Day21 : AdventSolution(2021, 21, "Dirac Dice") {
     override fun solvePartOne(input: String): Int {
         var (p1, p2) = parse(input)
         p1--
@@ -24,48 +24,38 @@ object Day21 : AdventSolution(2021, 21, "???") {
     }
 
     override fun solvePartTwo(input: String): Long {
-        val (p1, p2) = parse(input)
-        val initial = State(p1 - 1, p2 - 1, 0, 0)
-
-        var universes = mapOf(initial to 1L)
-        var p1Wins = 0L
-        var p2Wins = 0L
-
-        var p1Turn = true
+        val universes = mutableMapOf<State, WinCount>()
         val distribution3d3 = mapOf(3 to 1, 4 to 3, 5 to 6, 6 to 7, 7 to 6, 8 to 3, 9 to 1)
-        fun Map<State, Long>.takeTurn(isTurnForPlayer1: Boolean): Map<State, Long> {
-
-            return asSequence()
-                .flatMap { (oldState, count) ->
-                    distribution3d3.map { (rv, rc) ->
-                        if (isTurnForPlayer1) {
-                            val newPos = (oldState.pos1 + rv) % 10
-                            val newScore = oldState.score1 + newPos + 1
-                            oldState.copy(pos1 = newPos, score1 = newScore) to count * rc
-                        } else {
-                            val newPos = (oldState.pos2 + rv) % 10
-                            val newScore = oldState.score2 + newPos + 1
-                            oldState.copy(pos2 = newPos, score2 = newScore) to count * rc
-                        }
-                    }
+        fun winnings(s: State): WinCount = universes.getOrPut(s) {
+            when {
+                s.score1 >= 21 -> WinCount(1, 0)
+                s.score2 >= 21 -> WinCount(0, 1)
+                else -> distribution3d3.map { (roll, frequency) ->
+                    winnings(
+                        State(
+                            pos1 = s.pos2,
+                            pos2 = (s.pos1 + roll) % 10,
+                            score1 = s.score2,
+                            score2 = s.score1 + (s.pos1 + roll) % 10 + 1
+                        )
+                    ).flip() * frequency.toLong()
                 }
-                .groupingBy { it.first }
-                .fold(0) { acc, (_, v) -> acc + v }
+                    .fold(WinCount(0, 0), WinCount::plus)
+            }
         }
 
-        val winningScore = 21
-        while (universes.isNotEmpty()) {
-            universes = universes.takeTurn(p1Turn)
-            p1Wins += universes.filterKeys { it.score1 >= winningScore }.values.sum()
-            p2Wins += universes.filterKeys { it.score2 >= winningScore }.values.sum()
-            universes = universes.filterKeys { it.score1 < winningScore && it.score2 < winningScore }
-            p1Turn = !p1Turn
-        }
-
+        val (p1, p2) = parse(input)
+        val (p1Wins, p2Wins) = winnings(State(p1 - 1, p2 - 1, 0, 0))
         return maxOf(p1Wins, p2Wins)
     }
 
     private fun parse(input: String) = input.lines().map { it.last() - '0' }
+
+    private data class WinCount(val p1: Long, val p2: Long) {
+        operator fun plus(o: WinCount) = WinCount(p1 + o.p1, p2 + o.p2)
+        operator fun times(s: Long) = WinCount(p1 * s, p2 * s)
+        fun flip() = WinCount(p2, p1)
+    }
 
     private data class State(val pos1: Int, val pos2: Int, val score1: Int, val score2: Int)
 }
