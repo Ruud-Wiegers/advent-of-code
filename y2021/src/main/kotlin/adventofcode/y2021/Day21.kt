20ffd6485dfc14ed77664ea022a086749249aef0
Ruud Wiegers <ruud.wiegers@gmail.com>
2021-12-21 08:41:33 +0100

    Day 21

--- y2021/src/main/kotlin/adventofcode/y2021/Day21.kt
+++ y2021/src/main/kotlin/adventofcode/y2021/Day21.kt
@@ -1,25 +1,16 @@
 package adventofcode.y2021
 
 import adventofcode.AdventSolution
-import adventofcode.solve
-
-fun main() {
-    Day21.solve()
-}
 
 object Day21 : AdventSolution(2021, 21, "???") {
     override fun solvePartOne(input: String): Int {
         var (p1, p2) = parse(input)
-
         p1--
         p2--
-
         var p1Score = 0
         var p2Score = 0
-
         var dice = 1
         var rolls = 0
-
         fun next() = dice.also { rolls++;if (++dice > 100) dice = 1 }
         while (p1Score < 1000 && p2Score < 1000) {
             p1 += next() + next() + next()
@@ -42,7 +33,7 @@ object Day21 : AdventSolution(2021, 21, "???") {
 
         var p1Turn = true
 
-        fun roll() = universes.asSequence().flatMap { (oldState, count) ->
+        fun Map<State, Long>.roll() = asSequence().flatMap { (oldState, count) ->
             (1..3).map { roll ->
                 if (p1Turn) oldState.copy(pos1 = (oldState.pos1 + roll) % 10) to count
                 else oldState.copy(pos2 = (oldState.pos2 + roll) % 10) to count
@@ -50,31 +41,26 @@ object Day21 : AdventSolution(2021, 21, "???") {
         }.groupBy({ it.first }, { it.second })
             .mapValues { it.value.sum() }
 
-        fun score() = universes.mapKeys { (oldState) ->
-            if (p1Turn) oldState.copy(score1 = oldState.score1 + oldState.pos1+1)
-            else oldState.copy(score2 = oldState.score2 + oldState.pos2+1)
+        fun Map<State, Long>.score() = mapKeys { (oldState) ->
+            if (p1Turn) oldState.copy(score1 = oldState.score1 + oldState.pos1 + 1)
+            else oldState.copy(score2 = oldState.score2 + oldState.pos2 + 1)
 
         }
 
         while (universes.isNotEmpty()) {
-            universes = roll()
-            universes = roll()
-            universes = roll()
-            universes = score()
+            universes = universes.roll().roll().roll().score()
 
             p1Wins += universes.filterKeys { it.score1 >= 21 }.values.sum()
             p2Wins += universes.filterKeys { it.score2 >= 21 }.values.sum()
-            universes = universes.filterKeys { it.score1 <21 && it.score2 <21 }
+            universes = universes.filterKeys { it.score1 < 21 && it.score2 < 21 }
 
             p1Turn = !p1Turn
         }
 
-        return maxOf(p1Wins,p2Wins)
+        return maxOf(p1Wins, p2Wins)
     }
 
     private fun parse(input: String) = input.lines().map { it.last() - '0' }
 
     private data class State(val pos1: Int, val pos2: Int, val score1: Int, val score2: Int)
-
-
 }
