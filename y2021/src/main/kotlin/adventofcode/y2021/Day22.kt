4d00416385e989ced711c8a81f07dc78d70930aa
Ruud Wiegers <ruud.wiegers@topicus.nl>
2021-12-22 12:02:09 +0100

    Day 22

--- y2021/src/main/kotlin/adventofcode/y2021/Day22.kt
+++ y2021/src/main/kotlin/adventofcode/y2021/Day22.kt
@@ -25,7 +25,6 @@ object Day22 : AdventSolution(2021, 22, "Reactor Reboot") {
                 for (y in ys)
                     for (z in zs)
                         area -= Vec3(x, y, z)
-
         }
 
         parse(input).take(20).forEach { (on, c) ->
@@ -35,25 +34,17 @@ object Day22 : AdventSolution(2021, 22, "Reactor Reboot") {
         return area.size
     }
 
-
-    override fun solvePartTwo(input: String): Long {
-        val commands = parse(input).drop(20)
-        val cubes = mutableListOf<Cube>()
-
-        commands.forEach { (on, new) ->
+    override fun solvePartTwo(input: String) = parse(input)
+        .fold(listOf<Cube>()) { cubes, (on, new) ->
             if (on) {
-                val toAdd = cubes.fold(listOf(new)) { splitting, exist ->
-                    println(cubes.size)
-                    splitting.flatMap { it.splitBy(exist) }
-                        .filter { new -> new !in exist }
+                cubes + cubes.fold(listOf(new)) { splitting, exist ->
+                    splitting.flatMap { it.splitBy(exist) }.filter { new -> new !in exist }
                 }
-                cubes += toAdd
+            } else {
+                cubes.flatMap { splitting -> splitting.splitBy(new).filter { it !in new } }
             }
         }
-
-        return cubes.sumOf { it.size }
-    }
-
+        .sumOf { it.size }
 
     private fun parse(input: String) = input.lines().map {
         """(on|off) x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)""".toRegex()
@@ -66,43 +57,45 @@ object Day22 : AdventSolution(2021, 22, "Reactor Reboot") {
             )
         }
 
-}
+    data class Command(val on: Boolean, val cube: Cube)
+    data class Cube(val xs: IntRange, val ys: IntRange, val zs: IntRange) {
 
-private data class Command(val on: Boolean, val cube: Cube)
-private data class Cube(val xs: IntRange, val ys: IntRange, val zs: IntRange) {
+        val size = 1L * (xs.last - xs.first + 1) * (ys.last - ys.first + 1) * (zs.last - zs.first + 1)
 
-    val size = 1L * (xs.last - xs.first +1) * (ys.last - ys.first+1) * (zs.last - zs.first+1)
+        operator fun contains(o: Cube) =
+            o.xs.first in xs && o.xs.last in xs
+                    && o.ys.first in ys && o.ys.last in ys
+                    && o.zs.first in zs && o.zs.last in zs
 
-    operator fun contains(o: Cube) =
-        o.xs.first in xs && o.xs.last in xs
-                && o.ys.first in ys && o.ys.last in ys
-                && o.zs.first in zs && o.zs.last in zs
+        fun outside(o: Cube) =
+            (xs.last < o.xs.first) ||
+                    (ys.last < o.ys.first) ||
+                    (zs.last < o.zs.first) ||
+                    (o.xs.last < xs.first) ||
+                    (o.ys.last < ys.first) ||
+                    (o.zs.last < zs.first)
 
-    fun splitBy(o: Cube): List<Cube> {
-        if (xs.last < o.xs.first) return listOf(this)
-        if (ys.last < o.zs.first) return listOf(this)
-        if (zs.last < o.zs.first) return listOf(this)
-        if (o.xs.last < xs.first) return listOf(this)
-        if (o.ys.last < zs.first) return listOf(this)
-        if (o.zs.last < zs.first) return listOf(this)
-        return splitXBy(o).flatMap { it.splitYBy(o) }.flatMap { it.splitZBy(o) }
-    }
+        fun splitBy(o: Cube): List<Cube> = splitXBy(o).flatMap { it.splitYBy(o) }.flatMap { it.splitZBy(o) }
 
+        private fun splitXBy(o: Cube): List<Cube> = splitAxisBy(o, Cube::xs).map { copy(xs = it) }
+        private fun splitYBy(o: Cube): List<Cube> = splitAxisBy(o, Cube::ys).map { copy(ys = it) }
+        private fun splitZBy(o: Cube): List<Cube> = splitAxisBy(o, Cube::zs).map { copy(zs = it) }
 
-    private fun splitXBy(o: Cube): List<Cube> = splitAxisBy(o, Cube::xs).map { copy(xs = it) }
-    private fun splitYBy(o: Cube): List<Cube> = splitAxisBy(o, Cube::ys).map { copy(ys = it) }
-    private fun splitZBy(o: Cube): List<Cube> = splitAxisBy(o, Cube::zs).map { copy(zs = it) }
-
-    private inline fun splitAxisBy(o: Cube, dim: Cube.() -> IntRange): List<IntRange> {
-        return when {
-            o.dim().first > dim().first && o.dim().last < dim().last -> listOf(
-                dim().first until dim().first,
-                o.dim(),
-                o.dim().last + 1..dim().last
-            )
-            o.dim().first in dim() -> listOf(dim().first until o.dim().first, o.dim(), o.dim().first..dim().last)
-            o.dim().last in dim() -> listOf(dim().first..o.dim().last, o.dim().last + 1..dim().last)
-            else -> listOf(dim())
+        private inline fun splitAxisBy(o: Cube, dim: Cube.() -> IntRange): List<IntRange> {
+            return when {
+                this.outside(o)                                          -> listOf(dim())
+                o.dim().first > dim().first && o.dim().last < dim().last -> listOf(
+                    dim().first until o.dim().first,
+                    o.dim(),
+                    o.dim().last + 1..dim().last
+                )
+                o.dim().first in dim().first + 1..dim().last             -> listOf(
+                    dim().first until o.dim().first,
+                    o.dim().first..dim().last
+                )
+                o.dim().last in dim().first until dim().last             -> listOf(dim().first..o.dim().last, o.dim().last + 1..dim().last)
+                else                                                     -> listOf(dim())
+            }
         }
     }
 }
