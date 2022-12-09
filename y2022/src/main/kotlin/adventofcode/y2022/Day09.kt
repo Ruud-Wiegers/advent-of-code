877e332931f3312346db5cf94077ca2315a65667
Ruud Wiegers <ruud.wiegers@gmail.com>
2022-12-09 10:30:42 +0100

    Day 9

--- y2022/src/main/kotlin/adventofcode/y2022/Day09.kt
+++ y2022/src/main/kotlin/adventofcode/y2022/Day09.kt
@@ -5,37 +5,32 @@ import adventofcode.util.vector.Direction
 import adventofcode.util.vector.Vec2
 import kotlin.math.sign
 
-
 object Day09 : AdventSolution(2022, 9, "Rope Bridge") {
 
     override fun solvePartOne(input: String) = input.lineSequence()
-        .flatMap {
-            val (dir, len) = it.split(' ')
-            List(len.toInt()) { dir.toDirection() }
-        }
-        .map { it.vector }
+        .map { it.split(' ') }
+        .flatMap { (dir, len) -> List(len.toInt()) { dir.toDirection() } }
+        .map(Direction::vector)
         .scan(Vec2.origin, Vec2::plus)
-        .scan(Vec2.origin, ::tailMove)
+        .scan(Vec2.origin, ::moveTail)
         .distinct()
         .count()
 
 
     override fun solvePartTwo(input: String) = input.lineSequence()
-        .flatMap {
-            val (dir, len) = it.split(' ')
-            List(len.toInt()) { dir.toDirection() }
-        }
+        .map { it.split(' ') }
+        .flatMap { (dir, len) -> List(len.toInt()) { dir.toDirection() } }
         .map(Direction::vector)
         .scan(Vec2.origin, Vec2::plus)
-        .scan(Vec2.origin, ::tailMove)
-        .scan(Vec2.origin, ::tailMove)
-        .scan(Vec2.origin, ::tailMove)
-        .scan(Vec2.origin, ::tailMove)
-        .scan(Vec2.origin, ::tailMove)
-        .scan(Vec2.origin, ::tailMove)
-        .scan(Vec2.origin, ::tailMove)
-        .scan(Vec2.origin, ::tailMove)
-        .scan(Vec2.origin, ::tailMove)
+        .scan(Vec2.origin, ::moveTail)
+        .scan(Vec2.origin, ::moveTail)
+        .scan(Vec2.origin, ::moveTail)
+        .scan(Vec2.origin, ::moveTail)
+        .scan(Vec2.origin, ::moveTail)
+        .scan(Vec2.origin, ::moveTail)
+        .scan(Vec2.origin, ::moveTail)
+        .scan(Vec2.origin, ::moveTail)
+        .scan(Vec2.origin, ::moveTail)
         .distinct()
         .count()
 
@@ -47,10 +42,10 @@ object Day09 : AdventSolution(2022, 9, "Rope Bridge") {
         else -> throw IllegalArgumentException(this)
     }
 
-    private fun tailMove(prev: Vec2, head: Vec2): Vec2 {
-        val diff = head - prev
+    private fun moveTail(previousTail: Vec2, head: Vec2): Vec2 {
+        val diff = head - previousTail
         val step = Vec2(diff.x.sign, diff.y.sign)
-        return if (diff == step) prev else prev + step
+        return if (diff == step) previousTail else previousTail + step
     }
 }
 
