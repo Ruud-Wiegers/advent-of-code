6787fd7f4e13b97312b2691d55895b636d070ead
Ruud Wiegers <ruud.wiegers@gmail.com>
2022-12-09 11:22:13 +0100

    Day 9, merged part one and two

--- y2022/src/main/kotlin/adventofcode/y2022/Day09.kt
+++ y2022/src/main/kotlin/adventofcode/y2022/Day09.kt
@@ -7,30 +7,16 @@ import kotlin.math.sign
 
 object Day09 : AdventSolution(2022, 9, "Rope Bridge") {
 
-    override fun solvePartOne(input: String) = input.lineSequence()
+    override fun solvePartOne(input: String) = solution(input, 1)
+    override fun solvePartTwo(input: String) = solution(input, 9)
+
+    private fun solution(input: String, knots: Int) = input
+        .lineSequence()
         .map { it.split(' ') }
         .flatMap { (dir, len) -> List(len.toInt()) { dir.toDirection() } }
         .map(Direction::vector)
         .scan(Vec2.origin, Vec2::plus)
-        .scan(Vec2.origin, ::moveTail)
-        .distinct()
-        .count()
-
-
-    override fun solvePartTwo(input: String) = input.lineSequence()
-        .map { it.split(' ') }
-        .flatMap { (dir, len) -> List(len.toInt()) { dir.toDirection() } }
-        .map(Direction::vector)
-        .scan(Vec2.origin, Vec2::plus)
-        .scan(Vec2.origin, ::moveTail)
-        .scan(Vec2.origin, ::moveTail)
-        .scan(Vec2.origin, ::moveTail)
-        .scan(Vec2.origin, ::moveTail)
-        .scan(Vec2.origin, ::moveTail)
-        .scan(Vec2.origin, ::moveTail)
-        .scan(Vec2.origin, ::moveTail)
-        .scan(Vec2.origin, ::moveTail)
-        .scan(Vec2.origin, ::moveTail)
+        .tailPath(knots)
         .distinct()
         .count()
 
@@ -42,10 +28,12 @@ object Day09 : AdventSolution(2022, 9, "Rope Bridge") {
         else -> throw IllegalArgumentException(this)
     }
 
+    private fun Sequence<Vec2>.tailPath(knots: Int) =
+        generateSequence(this) { it.scan(Vec2.origin, ::moveTail) }.elementAt(knots)
+
     private fun moveTail(previousTail: Vec2, head: Vec2): Vec2 {
         val diff = head - previousTail
         val step = Vec2(diff.x.sign, diff.y.sign)
         return if (diff == step) previousTail else previousTail + step
     }
 }
-
