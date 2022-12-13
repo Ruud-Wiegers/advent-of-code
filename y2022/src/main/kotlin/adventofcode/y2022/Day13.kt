56b75c1cfcad5d8d6937ea9e5e417300663b6b7e
Ruud Wiegers <ruud.wiegers@gmail.com>
2022-12-13 20:56:41 +0100

    Day 13, more magic

--- y2022/src/main/kotlin/adventofcode/y2022/Day13.kt
+++ y2022/src/main/kotlin/adventofcode/y2022/Day13.kt
@@ -10,7 +10,6 @@ object Day13 : AdventSolution(2022, 13, "Distress Signal") {
         .filter { (_, v) -> v[0] < v[1] }
         .sumOf { it.index + 1 }
 
-
     override fun solvePartTwo(input: String): Int {
         val dividers = listOf("[[2]]", "[[6]]")
         return (dividers + input).joinToString("\n")
@@ -28,64 +27,51 @@ private fun parse(input: String): Sequence<Node> = input.lineSequence()
     .map(::tokenize)
     .map(::parseToNode)
 
-private fun tokenize(input: String) = sequence {
+private fun tokenize(input: String) = iterator {
     var remainder = input
 
     while (remainder.isNotEmpty()) {
         when (remainder[0]) {
-            '[' -> yield(Token.Push).also { remainder = remainder.drop(1) }
-            ']' -> yield(Token.Pop).also { remainder = remainder.drop(1) }
+            '[' -> yield("[").also { remainder = remainder.drop(1) }
+            ']' -> yield("]").also { remainder = remainder.drop(1) }
             ',' -> remainder = remainder.drop(1)
             else -> {
                 val s = remainder.takeWhile { it.isDigit() }
                 remainder = remainder.drop(s.length)
-                yield(Token.Num(s.toInt()))
+                yield(s)
             }
         }
     }
 }
 
-private fun parseToNode(tokens: Sequence<Token>): Node {
-    val stack = mutableListOf(Branch())
-    tokens.forEach {
-        when (it) {
-            Token.Push -> stack += Branch()
-            Token.Pop -> stack.removeLast().let { node -> stack.last().children += node }
-            is Token.Num -> stack.last().children += Leaf(it.n)
+private fun parseToNode(tokens: Iterator<String>): Node {
+    val nodes = mutableListOf<Node>()
+    while (tokens.hasNext()) {
+        when (val t = tokens.next()) {
+            "[" -> nodes += parseToNode(tokens)
+            "]" -> return Branch(nodes)
+            else -> nodes += Leaf(t.toInt())
         }
     }
-    return stack.first().children.first()
-}
-
-private sealed class Token {
-    object Push : Token()
-    object Pop : Token()
-    data class Num(val n: Int) : Token()
+    return nodes.single()
 }
 
 private sealed class Node : Comparable<Node>
 
 private data class Leaf(val i: Int) : Node() {
     override fun compareTo(other: Node): Int = when (other) {
-        is Leaf -> compareValues(i, other.i)
-        is Branch -> Branch(mutableListOf(this)).compareTo(other)
+        is Leaf -> i.compareTo(other.i)
+        is Branch -> Branch(listOf(this)).compareTo(other)
     }
 
     override fun toString() = i.toString()
 }
 
-private data class Branch(val children: MutableList<Node> = mutableListOf()) : Node() {
+private data class Branch(val children: List<Node>) : Node() {
     override fun compareTo(other: Node): Int = when (other) {
         is Leaf -> compareTo(Branch(mutableListOf(other)))
-        is Branch -> compareList(children, other.children)
-    }
-
-    private fun compareList(a: List<Node>, b: List<Node>): Int = when {
-        a.isEmpty() && b.isEmpty() -> 0
-        a.isEmpty() -> -1
-        b.isEmpty() -> 1
-        a[0].compareTo(b[0]) != 0 -> a[0].compareTo(b[0])
-        else -> compareList(a.drop(1), b.drop(1))
+        is Branch -> children.zip(other.children, Node::compareTo)
+            .find { it != 0 } ?: children.size.compareTo(other.children.size)
     }
 
     override fun toString() = children.joinToString(",", "[", "]")
