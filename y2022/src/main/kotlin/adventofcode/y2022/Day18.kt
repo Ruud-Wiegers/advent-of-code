53a8d967fe7c02f74566007e2f0710d5ff53f9ed
Ruud Wiegers <ruud.wiegers@gmail.com>
2022-12-18 08:55:14 +0100

    Day 18

--- y2022/src/main/kotlin/adventofcode/y2022/Day18.kt
+++ y2022/src/main/kotlin/adventofcode/y2022/Day18.kt
@@ -1,33 +1,28 @@
 package adventofcode.y2022
 
 import adventofcode.AdventSolution
-import adventofcode.solve
-import adventofcode.util.collections.cycle
-import adventofcode.util.collections.takeWhileDistinct
-import adventofcode.util.vector.Direction
-import adventofcode.util.vector.Vec2
 import adventofcode.util.vector.Vec3
 
 
 object Day18 : AdventSolution(2022, 18, "Boiling Boulders") {
 
     override fun solvePartOne(input: String): Int {
-        val parsed = parse(input).toList()
+        val lava = parse(input).toList()
 
-        val neighbors = parsed.flatMap { directions.map { d -> it + d } }.count { it in parsed }
+        val internalFaces = lava.flatMap { l -> directions.map(l::plus) }.count(lava::contains)
 
-        return (6 * parsed.size) - neighbors
+        return (6 * lava.size) - internalFaces
     }
 
     override fun solvePartTwo(input: String): Int {
-        val parsed = parse(input).toSet()
+        val lava = parse(input).toSet()
 
-        val xs = parsed.minOf { it.x } - 1..parsed.maxOf { it.x } + 1
-        val ys = parsed.minOf { it.y } - 1..parsed.maxOf { it.y } + 1
-        val zs = parsed.minOf { it.z } - 1..parsed.maxOf { it.z } + 1
+        //bounding box
+        val xs = lava.minOf { it.x } - 1..lava.maxOf { it.x } + 1
+        val ys = lava.minOf { it.y } - 1..lava.maxOf { it.y } + 1
+        val zs = lava.minOf { it.z } - 1..lava.maxOf { it.z } + 1
 
-
-
+        //all reachable cubes
         fun bfs(start: Vec3, reachableNeighbors: (Vec3) -> List<Vec3>): Set<Vec3> =
             generateSequence(Pair(setOf(start), setOf(start))) { (frontier, visited) ->
                 val unexploredNeighbors = frontier.flatMap(reachableNeighbors).toSet() - visited
@@ -36,19 +31,23 @@ object Day18 : AdventSolution(2022, 18, "Boiling Boulders") {
                 .takeWhile { (frontier, _) -> frontier.isNotEmpty() }
                 .last().second
 
-        val casing = bfs(Vec3.origin) { v->
-            directions.map { d->d+v }
-                .filter { it.x in xs && it.y in ys &&it.z in zs }
-                .filter { it !in parsed }
+        //flood-fill the outside
+        val casing = bfs(Vec3.origin) { v ->
+
+            //reachable if in box but not in lava
+            directions.map(v::plus)
+                .filterNot(lava::contains)
+                .filter { it.x in xs && it.y in ys && it.z in zs }
         }
 
-        return parsed.flatMap { directions.map { d -> it + d } }.count { it in casing }
-
-
+        //all faces that touch the casing are exposed
+        return lava.flatMap { l -> directions.map(l::plus) }.count(casing::contains)
     }
 
     private fun parse(input: String) =
-        input.lineSequence().map { it.split(',').map { it.toInt() } }.map { Vec3(it[0], it[1], it[2]) }
+        input.lineSequence()
+            .map { it.split(',').map(String::toInt) }
+            .map { Vec3(it[0], it[1], it[2]) }
 
     private val directions = listOf(
         Vec3(-1, 0, 0),
