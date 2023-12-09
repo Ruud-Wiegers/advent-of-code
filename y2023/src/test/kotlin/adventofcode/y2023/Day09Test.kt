ecff6ae4fbd89409d7fb7ffd229e7fc1ac96bfb6
Ruud Wiegers <ruud.wiegers@gmail.com>
2023-12-09 13:08:08 +0100

    Day 9

--- y2023/src/test/kotlin/adventofcode/y2023/Day09Test.kt
+++ y2023/src/test/kotlin/adventofcode/y2023/Day09Test.kt
@@ -7,16 +7,13 @@ internal class Day09Test {
 
     @Test
     fun solvePartOne() {
-        val input = """10 13 16 21 30 45"""
-
         assertEquals(68, Day09.solvePartOne(input))
     }
 
     @Test
     fun solvePartTwo() {
-        val input = """10 13 16 21 30 45""".trimIndent()
-
         assertEquals(5, Day09.solvePartTwo(input))
     }
 
+    private val input = "10 13 16 21 30 45"
 }
