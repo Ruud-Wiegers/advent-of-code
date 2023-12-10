faecc3825d7c0f1a1ce33a40ccd566d574cb9a6f
Ruud Wiegers <ruud.wiegers@gmail.com>
2023-12-10 17:13:51 +0100

    Day 10

--- y2023/src/main/kotlin/adventofcode/y2023/Day10.kt
+++ y2023/src/main/kotlin/adventofcode/y2023/Day10.kt
@@ -34,7 +34,6 @@ object Day10 : AdventSolution(2023, 10, "Pipe Maze") {
         val result = seq.drop(1).indexOfFirst { grid[it.first] == 'S' }
 
         return (result + 1) / 2
-
     }
 
     override fun solvePartTwo(input: String): Int {
@@ -47,7 +46,6 @@ object Day10 : AdventSolution(2023, 10, "Pipe Maze") {
 
         val connections = Direction.entries.filter { it.reverse in (grid[startPosition + it.vector]?.let { edges[it]} ?: emptyList()) }.toSet()
 
-        val startSymbol = edges.entries.first { it.value == connections }.key
 
         val seq = generateSequence(Pair(startPosition, connections.first())) { (pos, dir) ->
             val newPos = pos + dir.vector
@@ -58,79 +56,24 @@ object Day10 : AdventSolution(2023, 10, "Pipe Maze") {
 
         val pipes = seq.drop(1).takeWhile { grid[it.first] != 'S' }.map { it.first }.toSet()
 
+        val startSymbol = edges.entries.first { it.value == connections }.key
         val pipeMap = grid.filterKeys { it in pipes } + (startPosition to startSymbol)
 
-        return pipeMap.toGrid(' ').sumOf { row ->
+        //pretend we're standing on the 'lower half' of a tile, moving right.
+        //these are the pipe shapes where we cross from inside/outside the loop
+        val pipeCrossing = edges.filterValues { DOWN in it }.keys.joinToString("")
 
+        return pipeMap.toGrid(' ').flatMap { row ->
+
+            //scan the row, keeping track of whether we're on an open space and whether we're inside or outside the loop
             row.scan(Pair(true, false)) { (_, isInside), ch ->
-                Pair((ch == ' '), (ch in "|7F") xor isInside)
+                Pair(ch == ' ', if (ch in pipeCrossing) !isInside else isInside)
             }
-                .count { (open, inside) -> open && inside }
-
-
         }
-
-
+            .count { (isOpenSpace, isInside) -> isOpenSpace && isInside }
     }
 
 }
-
-/* failed attempt at flood fill
-
-      val leftSide = mutableSetOf<Vec2>()
-      val rightSide = mutableSetOf<Vec2>()
-      val pipe = mutableSetOf(start)
-
-      seq.drop(1).takeWhile { grid[it.first] != 'S' }.forEach { (position, facing) ->
-          when {
-              facing in grid[position]!!.edges() -> {
-                  leftSide += position + facing.turnLeft.vector
-                  rightSide += position + facing.turnRight.vector
-              }
-
-              facing.turnLeft in grid[position]!!.edges() -> {
-                  rightSide += listOf(position + facing.turnRight.vector, position + facing.vector)
-              }
-
-              facing.turnRight in grid[position]!!.edges() -> {
-                  leftSide += listOf(position + facing.turnLeft.vector, position + facing.vector)
-              }
-
-          }
-
-          pipe += position
-      }
-
-
-
-
-      //TODO include edges of starting point ... not needed usually
-
-      val inside: MutableSet<Vec2> = leftSide.takeIf { it.all(grid::contains) } ?: rightSide
-
-
-      val open = inside.filter { it !in pipe }.toMutableSet()
-
-
-      while (true) {
-          val new = open.flatMap(Vec2::neighbors).filter { it !in open && it !in pipe }
-          open += new
-
-          println(open.size)
-          if (new.isEmpty()) break
-      }
-
-
-      val show: SparseGrid<Char> = grid.mapValues { (k, v) -> if (k in open) 'I' else if (k !in pipe) ' ' else '█' }
-
-
-      show.toGrid(" ").forEach { println(it.joinToString("")) }
-
- return open.size
-
-       */
-
-
 
 private val edges = mapOf(
     '|' to setOf(UP, DOWN),
@@ -140,5 +83,5 @@ private val edges = mapOf(
     '7' to setOf(DOWN, LEFT),
     'F' to setOf(DOWN, RIGHT),
     '.' to emptySet(),
-    'S' to setOf(UP,DOWN,LEFT,RIGHT)
+    'S' to setOf(UP, DOWN, LEFT, RIGHT) //ugly hack for last step in traversal
 )
