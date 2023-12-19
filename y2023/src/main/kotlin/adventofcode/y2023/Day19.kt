504961f78c3680bd69ba7922ebf8a67aed890938
Ruud Wiegers <ruud.wiegers@gmail.com>
2023-12-19 15:33:03 +0100

    Day 19 some cleanup

--- y2023/src/main/kotlin/adventofcode/y2023/Day19.kt
+++ y2023/src/main/kotlin/adventofcode/y2023/Day19.kt
@@ -9,95 +9,80 @@ fun main() {
 
 object Day19 : AdventSolution(2023, 19, "Aplenty") {
 
-    override fun solvePartOne(input: String): Long {
+    override fun solvePartOne(input: String): Int {
         val (workflows, parts) = parse(input)
 
         val flows = workflows.associateBy { it.name }
 
-
-        fun accepted(part: Part): Boolean {
-            var current: Target = Target.Flow("in")
-
-            while (current is Target.Flow) {
-                current = flows[current.name]!!.eval(part)
-            }
-
-            return current == Target.Accept
-        }
-
-        val result = parts.filter { accepted(it) }.sumOf { it.x + it.m + it.a + it.s }
-
-        return result.toLong()
+        return parts.filter { it.acceptedBy(flows) }.sumOf(Part::value)
     }
 
     override fun solvePartTwo(input: String): Long {
         val workflows = parse(input).first.associateBy { it.name }
 
-        val initial = PartRange(Part(1, 1, 1, 1), Part(4001, 4001, 4001, 4001))
+        val initial = PartRange(Part("xmas".map { it to 1 }.toMap()), Part("xmas".map { it to 4001 }.toMap()))
 
-
-        val incomplete = mutableListOf<Pair<PartRange, Target>>(initial to Target.Flow("in"))
+        val incomplete = mutableListOf<Pair<PartRange, Evaluation>>(initial to Evaluation.DecideByWorkflow("in"))
         val accepted = mutableListOf<PartRange>()
 
         while (incomplete.isNotEmpty()) {
             val (currentRange, currentTarget) = incomplete.removeLast()
 
             when (currentTarget) {
-                Target.Accept -> accepted += currentRange
-                Target.Reject -> continue
-                is Target.Flow -> {
-                    incomplete += workflows.getValue(currentTarget.name).eval(currentRange)
+                is Evaluation.DecideByWorkflow -> {
+                    incomplete += workflows.getValue(currentTarget.name).evaluatePartRange(currentRange)
                 }
+                Evaluation.Accepted -> accepted += currentRange
+                Evaluation.Rejected -> continue
             }
         }
 
-        return accepted.sumOf(PartRange::count)
-
+        return accepted.sumOf(PartRange::countPartsWithinRange)
     }
-
 }
 
 
 private fun parse(input: String): Pair<List<Workflow>, List<Part>> {
+
+    fun parsePart(input: String): Part {
+        val values = """(\d+)""".toRegex().findAll(input).map { it.value.toInt() }.toList()
+        return "xmas".asIterable().zip(values).toMap().let (::Part)
+    }
+
+    fun parseTarget(input: String) = when (input) {
+        "A" -> Evaluation.Accepted
+        "R" -> Evaluation.Rejected
+        else -> Evaluation.DecideByWorkflow(input)
+    }
+
+    fun parseWorkflow(input: String): Workflow {
+        val name = input.substringBefore("{")
+        val flows = input.substringAfter("{").dropLast(1).split(",")
+        val default = flows.last().let(::parseTarget)
+
+        val comparisons = flows.dropLast(1).map {
+            val property = it.first()
+            val lessThan = it[1] == '<'
+            val amount = it.substring(2).substringBefore(":").toInt()
+            val target = it.substringAfter(":").let(::parseTarget)
+            Comparison(property, lessThan, amount, target)
+        }
+
+        return Workflow(name, comparisons, default)
+    }
+
+
     val (workflows, parts) = input.split("\n\n")
 
     return workflows.lines().map { parseWorkflow(it) } to parts.lines().map { parsePart(it) }
 }
 
-private fun parseWorkflow(input: String): Workflow {
-    val name = input.substringBefore("{")
-    val flows = input.substringAfter("{").dropLast(1).split(",")
-    val default = flows.last().let(::parseTarget)
 
-    val comparisons = flows.dropLast(1).map {
-        val property = it.first()
-        val lessThan = it[1] == '<'
-        val amount = it.substring(2).substringBefore(":").toInt()
-        val target = it.substringAfter(":").let(::parseTarget)
-        Comparison(property, lessThan, amount, target)
-    }
+private data class Workflow(val name: String, val steps: List<Comparison>, val default: Evaluation) {
+    fun evaluatePartRange(part: Part): Evaluation = steps.firstNotNullOfOrNull { it.eval(part) } ?: default
 
-    return Workflow(name, comparisons, default)
-}
-
-private fun parseTarget(input: String) = when (input) {
-    "A" -> Target.Accept
-    "R" -> Target.Reject
-    else -> Target.Flow(input)
-}
-
-private fun parsePart(input: String): Part {
-    val (x, m, a, s) = """(\d+)""".toRegex().findAll(input).map { it.value.toInt() }.toList()
-    return Part(x, m, a, s)
-}
-
-
-private data class Workflow(val name: String, val steps: List<Comparison>, val default: Target) {
-    fun eval(part: Part): Target = steps.firstNotNullOfOrNull { it.eval(part) } ?: default
-
-    fun eval(partRange: PartRange): List<Pair<PartRange, Target>> {
-
-        val resolved = mutableListOf<Pair<PartRange, Target>>()
+    fun evaluatePartRange(partRange: PartRange): List<Pair<PartRange, Evaluation>> {
+        val resolved = mutableListOf<Pair<PartRange, Evaluation>>()
         val remainder = steps.fold(partRange) { range, comparison ->
             val (res, rem) = comparison.eval(range)
             resolved += res
@@ -108,51 +93,47 @@ private data class Workflow(val name: String, val steps: List<Comparison>, val d
     }
 }
 
-private data class Comparison(val property: Char, val lessThan: Boolean, val v: Int, val target: Target) {
+private data class Comparison(val property: Char, val lessThan: Boolean, val v: Int, val evaluation: Evaluation) {
 
-    fun eval(part: Part): Target? {
-        val p = select(part)
+    fun eval(part: Part): Evaluation? {
 
-        return if (lessThan && p < v) target
-        else if (!lessThan && p > v) target
+        return if (lessThan && part[property] < v) evaluation
+        else if (!lessThan && part[property] > v) evaluation
         else null
     }
 
-    fun eval(range: PartRange): Pair<Pair<PartRange, Target>, PartRange> =
+    fun eval(range: PartRange): Pair<Pair<PartRange, Evaluation>, PartRange> =
         if (lessThan) {
             val (low, high) = range.partition(property, v)
-            Pair(low to target, high)
+            Pair(low to evaluation, high)
         } else {
             val (low, high) = range.partition(property, v + 1)
-            Pair(high to target, low)
+            Pair(high to evaluation, low)
         }
 
-    private fun select(part: Part): Int = when (property) {
-            'x' -> part.x
-            'm' -> part.m
-            'a' -> part.a
-            's' -> part.s
-            else -> error("")
-        }
 }
 
-private sealed class Target {
-    data object Accept : Target()
-    data object Reject : Target()
-    data class Flow(val name: String) : Target()
+private sealed class Evaluation {
+    data object Accepted : Evaluation()
+    data object Rejected : Evaluation()
+    data class DecideByWorkflow(val name: String) : Evaluation()
 }
 
 
-private data class Part(val x: Int, val m: Int, val a: Int, val s: Int) {
-    fun copyWith(property: Char, value: Int) = when (property) {
-        'x' -> copy(x = value)
-        'm' -> copy(m = value)
-        'a' -> copy(a = value)
-        's' -> copy(s = value)
-        else -> error("unknown property")
-    }
+private data class Part(private val props:Map<Char,Int>) {
+    fun copyWith(property: Char, value: Int) = Part(props + (property to value))
 
-    operator fun minus(o: Part) = Part(o.x - x, o.m - m, o.a - a, o.s - s)
+    operator fun get(property: Char) = props.getValue(property)
+
+    fun value() = props.values.sum()
+    fun area() = props.values.fold(1L,Long::times)
+    operator fun minus(o:Part) = Part(o.props.mapValues { (p,v)-> v - this[p] })
+
+
+    fun acceptedBy(workflows: Map<String, Workflow>): Boolean = generateSequence<Evaluation>(Evaluation.DecideByWorkflow("in")) {
+        (it as? Evaluation.DecideByWorkflow)?.name?.let(workflows::get)?.evaluatePartRange(this)
+    }.last() == Evaluation.Accepted
+
 }
 
 private data class PartRange(val start: Part, val endExclusive: Part) {
@@ -160,9 +141,8 @@ private data class PartRange(val start: Part, val endExclusive: Part) {
     fun partition(property: Char, startOfHigh: Int): Pair<PartRange, PartRange> {
         val lower = copy(endExclusive = endExclusive.copyWith(property, startOfHigh))
         val higher = copy(start = start.copyWith(property, startOfHigh))
-
         return lower to higher
     }
 
-    fun count() = (endExclusive - start).let { (x, m, a, s) -> 1L * x * m * a * s }
+    fun countPartsWithinRange() = (endExclusive - start).area()
 }
