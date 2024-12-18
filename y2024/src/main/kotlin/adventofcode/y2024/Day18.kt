3b6f790d31405c2d36956ed71a578dabeb8d6a5d
Ruud Wiegers <ruud.wiegers@topicus.nl>
2024-12-18 15:08:09 +0100

    Day 18

--- y2024/src/main/kotlin/adventofcode/y2024/Day18.kt
+++ y2024/src/main/kotlin/adventofcode/y2024/Day18.kt
@@ -5,7 +5,6 @@ import adventofcode.util.vector.Vec2
 import adventofcode.util.vector.neighbors
 import adventofcode.util.vector.xBounds
 import adventofcode.util.vector.yBounds
-import kotlin.math.abs
 
 fun main() {
     Day18.solve()
@@ -19,14 +18,14 @@ object Day18 : AdventSolution(2024, 18, "RAM Run") {
         val bounds = Box(list.xBounds(), list.yBounds())
         val walls = list.take(1024).toSet()
 
-        var open = setOf(Vec2(0, 0))
         var count = 0
-        val seen = open.toMutableSet()
+        var open = setOf(Vec2(0, 0))
+        val closed = open.toMutableSet()
         val end = Vec2(bounds.xBounds.last, bounds.yBounds.last)
         while (end !in open) {
-            val candidates = open.flatMap(Vec2::neighbors).filter(bounds::contains).filterNot(seen::contains)
+            val candidates = open.flatMap(Vec2::neighbors).filter(bounds::contains).filterNot(closed::contains)
                 .filterNot(walls::contains).toSet()
-            seen += candidates
+            closed += candidates
             open = candidates
             count++
         }
@@ -37,15 +36,14 @@ object Day18 : AdventSolution(2024, 18, "RAM Run") {
 
     override fun solvePartTwo(input: String): String {
         val list = input.parseInput()
-
         val bounds = Box(list.xBounds(), list.yBounds())
-
         return binarySearch(bounds, list)
     }
 
-    private fun String.parseInput(): List<Vec2> = lines().map {
-        it.split(",").map(String::toInt).let { (x, y) -> Vec2(x, y) }
-    }
+}
+
+private fun String.parseInput(): List<Vec2> = lines().map {
+    it.split(",").map(String::toInt).let { (x, y) -> Vec2(x, y) }
 }
 
 private fun binarySearch(bounds: Box, list: List<Vec2>): String {
@@ -64,16 +62,15 @@ private fun binarySearch(bounds: Box, list: List<Vec2>): String {
 }
 
 private fun canEscape(bounds: Box, walls: Set<Vec2>): Boolean {
-    var open = setOf(Vec2(0, 0))
-    var count = 0
-    val seen = open.toMutableSet()
-    val end = Vec2(bounds.xBounds.last, bounds.yBounds.last)
-    while (end !in open && open.isNotEmpty()) {
-        val candidates = open.flatMap(Vec2::neighbors).filter(bounds::contains).filterNot(seen::contains)
+    val start = Vec2(0, 0)
+    val goal = Vec2(bounds.xBounds.last, bounds.yBounds.last)
+    var open = setOf(start)
+    val closed = open.toMutableSet()
+    while (goal !in open && open.isNotEmpty()) {
+        val candidates = open.flatMap(Vec2::neighbors).filter(bounds::contains).filterNot(closed::contains)
             .filterNot(walls::contains).toSet()
-        seen += candidates
+        closed += candidates
         open = candidates
-        count++
     }
 
     return open.isNotEmpty()
