d91f517e24612a657a44bb7ee454af6c2a4482b6
Ruud Wiegers <ruud.wiegers@gmail.com>
2024-12-22 08:42:44 +0100

    Day22

--- y2024/src/main/kotlin/adventofcode/y2024/Day22.kt
+++ y2024/src/main/kotlin/adventofcode/y2024/Day22.kt
@@ -8,49 +8,35 @@ fun main() {
 
 object Day22 : AdventSolution(2024, 22, "???") {
 
-    override fun solvePartOne(input: String): Long {
-        return input.lines().sumOf { generateSequence(it.toLong()) { calculate(it) }.elementAt(2000) }
+    override fun solvePartOne(input: String) = input.lines().sumOf {
+        generateSequence(it.toLong()) { calculate(it) }.elementAt(2000)
     }
 
     override fun solvePartTwo(input: String): Int {
-        val buyers = input.lines().map {
+        val bananaPrices = input.lines().map {
             generateSequence(it.toLong()) { calculate(it) }.drop(1).take(2000).map { (it % 10).toInt() }.toList()
         }
 
-        val buyerDeltas = buyers.map { it.zipWithNext { a, b -> b - a }.windowed(4) }
+        val deltaPatterns = bananaPrices.map { it.zipWithNext { a, b -> b - a }.windowed(4) }
 
-        fun score(pattern: List<Int>): Int {
-            return buyerDeltas.mapIndexed { index, v ->
-                val i = v.indexOf(pattern)
-
-                if (i < 0) 0 else buyers[index][i + 4]
-            }.sum()
+        val priceAtFirstOccurrence = deltaPatterns.zip(bananaPrices) { deltas, buyer ->
+            deltas.zip(buyer.drop(4))
+                .groupBy({ it.first }, { it.second })
+                .mapValues { it.value.first() }
+                .entries
         }
 
-        println(patterns().size0)
-var count = 0
-        return patterns().maxOf { score(it) .also { println("${count++}, $it") } }
-
+        return priceAtFirstOccurrence
+            .flatten()
+            .groupBy({ it.key }, { it.value })
+            .values
+            .maxOf { it.sum() }
     }
 
-    private fun calculate(input: Long): Long {
-        var step = input
-        step = (step xor step * 64) % 16777216
-        step = (step xor step / 32) % 16777216
-        step = (step xor step * 2048) % 16777216
-        return step
-    }
+}
 
-    private fun patterns() = buildList {
-        for (a in -9..9)
-            for (b in -9..9)
-                for (c in -9..9)
-                    for (d in -9..9) {
-                        if (a+b in -9..9 && a+b+c in -9..9 && a+b+c+d in 1..9)
-                            add(listOf(a, b, c, d))
-                    }
-
-    }.sortedByDescending(List<Int>::sum)
-
-
+private fun calculate(input: Long): Long {
+    val one = (input xor input * 64) % 16777216
+    val two = (one xor one / 32) % 16777216
+    return (two xor two * 2048) % 16777216
 }
--- y2024/src/test/kotlin/adventofcode/y2024/Day22Test.kt
+++ y2024/src/test/kotlin/adventofcode/y2024/Day22Test.kt
@@ -7,16 +7,11 @@ internal class Day22Test {
 
     @Test
     fun solvePartOne() {
-        assertEquals(37327623, Day22.solvePartOne(input))
+        assertEquals(37327623, Day22.solvePartOne("1\n10\n100\n2024"))
     }
 
     @Test
     fun solvePartTwo() {
-        assertEquals(23, Day22.solvePartTwo(input))
+        assertEquals(23, Day22.solvePartTwo("1\n2\n3\n2024"))
     }
-
-    private val input = """1
-10
-100
-2024"""
 }
