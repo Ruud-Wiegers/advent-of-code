6cffd10377dac3de6d8956858b6002c9dcd1669b
Ruud Wiegers <ruud.wiegers@topicus.nl>
2024-12-22 12:08:38 +0100

    Day 22, fiddling

--- y2024/src/main/kotlin/adventofcode/y2024/runAll.kt
+++ y2024/src/main/kotlin/adventofcode/y2024/runAll.kt
@@ -11,7 +11,7 @@ fun main() {
                 Day06, Day07, Day08, Day09, Day10,
                 Day11, Day12, Day13, Day14, Day15,
                 Day16, Day17, Day18, Day19, Day20,
-                Day21, // Day22, Day23, Day24, Day25
+                Day21, Day22, //Day23, Day24, Day25
             )
                 .forEach(AdventSolution::solve)
         }.let { println(formattedTime(it)) }
