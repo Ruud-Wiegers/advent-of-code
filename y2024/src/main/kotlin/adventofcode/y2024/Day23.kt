c9a8a176a7f32ab290d053b88a1149a0b43edf38
Ruud Wiegers <ruud.wiegers@gmail.com>
2024-12-23 08:35:54 +0100

    Day 23

--- y2024/src/main/kotlin/adventofcode/y2024/Day23.kt
+++ y2024/src/main/kotlin/adventofcode/y2024/Day23.kt
@@ -1,27 +1,36 @@
 package adventofcode.y2024
 
 import adventofcode.io.AdventSolution
-import kotlin.math.abs
 
 fun main() {
     Day23.solve()
 }
 
-object Day23 : AdventSolution(2024, 23, "???") {
+object Day23 : AdventSolution(2024, 23, "LAN Party") {
 
-    override fun solvePartOne(input: String): Int {
-        val parsed = input.parseInput()
+    override fun solvePartOne(input: String): Int =
+        clustering(input).elementAt(2).count { it.any { c -> c.startsWith("t") } }
 
-        return 0
+    override fun solvePartTwo(input: String): String =
+        clustering(input).last().single().joinToString(",")
+
+    private fun clustering(input: String): Sequence<List<List<String>>> {
+        val connections = input.parseInput()
+
+        fun next(groups: List<List<String>>) = groups.flatMap { old ->
+            connections.keys
+                .filter { candidate ->
+                    old.last() < candidate && old.all { oc -> oc in connections[candidate].orEmpty() }
+                }
+                .map { old + it }
+        }.takeIf { it.isNotEmpty() }
+
+        val initial = connections.keys.map { listOf(it) }
+        return generateSequence(initial, ::next)
     }
 
-    override fun solvePartTwo(input: String): Int {
-        val parsed = input.parseInput()
-
-        return 0
-    }
-
-    fun String.parseInput() = lines().map {
-        it.split("   ").map(String::toInt)
-    }
+    fun String.parseInput() = lineSequence().flatMap {
+        val list = it.split("-")
+        listOf(list, list.reversed())
+    }.groupBy({ it[0] }, { it[1] }).mapValues { it.value.toSet() }
 }
--- y2024/src/test/kotlin/adventofcode/y2024/Day23Test.kt
+++ y2024/src/test/kotlin/adventofcode/y2024/Day23Test.kt
@@ -7,13 +7,44 @@ internal class Day23Test {
 
     @Test
     fun solvePartOne() {
-        assertEquals(0, Day23.solvePartOne(input))
+        assertEquals(7, Day23.solvePartOne(input))
     }
 
     @Test
     fun solvePartTwo() {
-        assertEquals(0, Day23.solvePartTwo(input))
+        assertEquals("co,de,ka,ta", Day23.solvePartTwo(input))
     }
 
-    private val input = """0"""
+    private val input = """kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn"""
 }
