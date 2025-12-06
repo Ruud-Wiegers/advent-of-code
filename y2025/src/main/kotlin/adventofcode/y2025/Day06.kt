c33ef8cf4c7dae6cea031e2e82c497a8ca085651
Ruud Wiegers <ruud.wiegers@topicus.nl>
2025-12-06 08:01:27 +0100

    day 6

--- y2025/src/main/kotlin/adventofcode/y2025/Day06.kt
+++ y2025/src/main/kotlin/adventofcode/y2025/Day06.kt
@@ -2,9 +2,7 @@ package adventofcode.y2025
 
 import adventofcode.io.AdventSolution
 import adventofcode.util.algorithm.transposeString
-import adventofcode.util.collections.splitAfter
 import adventofcode.util.collections.splitBefore
-import kotlin.math.abs
 
 fun main() {
     Day06.solve()
@@ -12,38 +10,38 @@ fun main() {
 
 object Day06 : AdventSolution(2025, 6, "Trash Compactor") {
 
-    override fun solvePartOne(input: String): Any {
+    override fun solvePartOne(input: String) = solve(input) { parseToProblem(it.transposeString()) }
+    override fun solvePartTwo(input: String) = solve(input) { parseToTransposedProblem(it) }
 
-        return parse(input).sumOf {
-            when(it.operator) {
-                '+' -> it.numbers.sum()
-                '*' -> it.numbers.reduce(Long::times)
-                else -> throw IllegalArgumentException("Unknown operator ${it.operator}")
-            }
-        }
-    }
-
-    override fun solvePartTwo(input: String): Int {
-        return TODO()
-    }
-
-    private fun parse(input: String): List<Problem> {
+    private fun solve(input: String, mapping: (List<String>) -> Problem): Long {
         val lines = input.lines()
-
         val padTo = lines.maxOf { it.length }
         val padded = lines.map { it.padEnd(padTo, ' ') }
-
         return padded.transposeString()
             .splitBefore { it.last() in "*+" }
-            .map { parseToProblem(it.transposeString()) }
+            .map { mapping(it) }
+            .sumOf {
+                when (it.operator) {
+                    '+' -> it.numbers.sum()
+                    '*' -> it.numbers.reduce(Long::times)
+                    else -> error("")
+                }
+            }
     }
+}
 
-    private fun parseToProblem(input: List<String>): Problem {
-        val numbers = input.dropLast(1).map { it.trim().toLong()}
-        val operator = input.last().trim()[0]
+private fun parseToProblem(input: List<String>): Problem {
+    val numbers = input.dropLast(1).map { it.trim().toLong() }
+    val operator = input.last().trim()[0]
 
-        return Problem(numbers,operator)
-    }
+    return Problem(numbers, operator)
+}
+
+private fun parseToTransposedProblem(input: List<String>): Problem {
+    val numbers = input.filter { it.isNotBlank() }.map { it.dropLast(1).trim().toLong() }
+    val operator = input.first().last()
+
+    return Problem(numbers, operator)
 }
 
 private data class Problem(val numbers: List<Long>, val operator: Char)
--- y2025/src/test/kotlin/adventofcode/y2025/Day06Test.kt
+++ y2025/src/test/kotlin/adventofcode/y2025/Day06Test.kt
@@ -7,13 +7,16 @@ internal class Day06Test {
 
     @Test
     fun solvePartOne() {
-        assertEquals(TODO(), Day06.solvePartOne(input))
+        assertEquals(4277556, Day06.solvePartOne(input))
     }
 
     @Test
     fun solvePartTwo() {
-        assertEquals(TODO(), Day06.solvePartTwo(input))
+        assertEquals(3263827, Day06.solvePartTwo(input))
     }
 
-    private val input = """"""
+    private val input = """123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   + """
 }
