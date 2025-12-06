cecfbfa83fdc0845823a3b634da41b6d0a2d179b
Ruud Wiegers <ruud.wiegers@topicus.nl>
2025-12-06 08:27:26 +0100

    cleanup

--- y2025/src/main/kotlin/adventofcode/y2025/Day06.kt
+++ y2025/src/main/kotlin/adventofcode/y2025/Day06.kt
@@ -13,35 +13,33 @@ object Day06 : AdventSolution(2025, 6, "Trash Compactor") {
     override fun solvePartOne(input: String) = solve(input) { parseToProblem(it.transposeString()) }
     override fun solvePartTwo(input: String) = solve(input) { parseToTransposedProblem(it) }
 
-    private fun solve(input: String, mapping: (List<String>) -> Problem): Long {
+    private fun solve(input: String, mapping: (transposedInput: List<String>) -> Problem): Long {
         val lines = input.lines()
         val padTo = lines.maxOf { it.length }
         val padded = lines.map { it.padEnd(padTo, ' ') }
-        return padded.transposeString()
+
+        val problems = padded.transposeString()
             .splitBefore { it.last() in "*+" }
             .map { mapping(it) }
-            .sumOf {
-                when (it.operator) {
-                    '+' -> it.numbers.sum()
-                    '*' -> it.numbers.reduce(Long::times)
-                    else -> error("")
-                }
-            }
+
+        return problems.sumOf(Problem::solve)
     }
 }
 
-private fun parseToProblem(input: List<String>): Problem {
-    val numbers = input.dropLast(1).map { it.trim().toLong() }
-    val operator = input.last().trim()[0]
+private fun parseToProblem(input: List<String>) = Problem(
+    numbers = input.dropLast(1).map { it.trim().toLong() },
+    operator = input.last().trim().first()
+)
 
-    return Problem(numbers, operator)
+private fun parseToTransposedProblem(transposedInput: List<String>) = Problem(
+    numbers = transposedInput.filter { it.isNotBlank() }.map { it.dropLast(1).trim().toLong() },
+    operator = transposedInput.first().last()
+)
+
+private data class Problem(val numbers: List<Long>, val operator: Char) {
+    fun solve() = when (operator) {
+        '+' -> numbers.reduce(Long::plus)
+        '*' -> numbers.reduce(Long::times)
+        else -> error(operator)
+    }
 }
-
-private fun parseToTransposedProblem(input: List<String>): Problem {
-    val numbers = input.filter { it.isNotBlank() }.map { it.dropLast(1).trim().toLong() }
-    val operator = input.first().last()
-
-    return Problem(numbers, operator)
-}
-
-private data class Problem(val numbers: List<Long>, val operator: Char)
