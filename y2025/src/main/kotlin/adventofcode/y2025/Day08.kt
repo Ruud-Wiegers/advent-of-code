7978daf51c648767205fa08ef92a7b949335d7dc
Ruud Wiegers <ruud.wiegers@topicus.nl>
2025-12-08 20:09:51 +0100

    ever more fiddling

--- y2025/src/main/kotlin/adventofcode/y2025/Day08.kt
+++ y2025/src/main/kotlin/adventofcode/y2025/Day08.kt
@@ -14,11 +14,12 @@ object Day08 : AdventSolution(2025, 8, "Playground") {
 
     fun solvePartOne(input: String, connectionCount: Int): Long {
         val junctions = parse(input)
-
-        val distances = distances(junctions).take(connectionCount)
+        val connections = distances(junctions).sortedBy { it.distance }.take(connectionCount)
 
         val unionFind = UnionFind(junctions.size)
-        distances.forEach { unionFind.union(it.fromIndex, it.toIndex) }
+        for ((fromIndex, toIndex) in connections) {
+            unionFind.union(fromIndex, toIndex)
+        }
 
         val results = unionFind.toSets()
         val largestGroupSizes = results.map { it.size }.sortedDescending().take(3)
@@ -27,8 +28,8 @@ object Day08 : AdventSolution(2025, 8, "Playground") {
 
     override fun solvePartTwo(input: String): Long {
         val junctions = parse(input)
+        val distances = distances(junctions).sortedBy { it.distance }
 
-        val distances = distances(junctions)
         val sets = UnionFind(junctions.size)
 
         distances.forEach { (fromId, toId) ->
@@ -55,7 +56,7 @@ private fun distances(junctions: List<Vec3>): List<Connection> = buildList {
     for (a in junctions.indices)
         for (b in (a + 1)..junctions.lastIndex)
             add(Connection(a, b, magnitude(junctions[a], junctions[b])))
-}.sortedBy { it.distance }
+}
 
 fun magnitude(v1: Vec3, v2: Vec3): Long =
     (v2 - v1).let { (x, y, z) -> x.toLong() * x + y.toLong() * y + z.toLong() * z }
