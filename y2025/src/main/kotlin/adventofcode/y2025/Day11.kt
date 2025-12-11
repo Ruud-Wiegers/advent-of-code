14b767b8bd962b79302eb8eb61b74caf825ddd10
Ruud Wiegers <ruud.wiegers@gmail.com>
2025-12-11 08:50:20 +0100

    stuff

--- y2025/src/main/kotlin/adventofcode/y2025/Day11.kt
+++ y2025/src/main/kotlin/adventofcode/y2025/Day11.kt
@@ -1,20 +1,80 @@
 package adventofcode.y2025
 
 import adventofcode.io.AdventSolution
-import kotlin.math.abs
 
 fun main() {
     Day11.solve()
 }
 
-object Day11 : AdventSolution(2025, 11, "???") {
+object Day11 : AdventSolution(2025, 11, "Reactor") {
 
-    override fun solvePartOne(input: String): Any {
-
-        return TODO()
+    override fun solvePartOne(input: String): Long {
+        val fullTree = parse(input)
+        return countPaths(fullTree, "you")["out"]!!
     }
 
-    override fun solvePartTwo(input: String): Int {
-        return TODO()
+    override fun solvePartTwo(input: String): Long {
+        val fullTree = parse(input)
+
+        val fromSvr = countPaths(fullTree, "svr")
+        val fromDac = countPaths(fullTree, "dac")
+        val fromFft = countPaths(fullTree, "fft")
+
+        val svrToDac = fromSvr["dac"] ?: 0L
+        val dacToFft = fromDac["fft"] ?: 0L
+        val fftToOut = fromFft["out"] ?: 0L
+        val path1 = svrToDac * dacToFft * fftToOut
+
+        val svrToFft = fromSvr["fft"] ?: 0L
+        val fftToDac = fromFft["dac"] ?: 0L
+        val dacToOut = fromDac["out"] ?: 0L
+        val path2 = svrToFft * fftToDac * dacToOut
+
+        return path1 + path2
     }
+
 }
+
+private fun parse(input: String): Map<String, List<String>> = input.lines().associate { line ->
+    val (node, links) = line.split(": ")
+    node to links.split(" ")
+}
+
+
+private fun countPaths(fullTree: Map<String, List<String>>, start: String): Map<String, Long> {
+    val forwards = fullTree.pruneUnreachable(start)
+    val backwards = forwards.invert()
+
+    val paths = mutableMapOf(start to 1L)
+    val open = forwards[start].orEmpty().toMutableSet()
+
+    while (open.isNotEmpty()) {
+        val candidate = open.first {
+            backwards[it].orEmpty().all { feed -> feed in paths }
+        }
+        open.remove(candidate)
+        paths[candidate] = backwards[candidate].orEmpty().sumOf { paths.getValue(it) }
+        open += forwards[candidate].orEmpty()
+    }
+
+    return paths
+}
+
+private fun Map<String, List<String>>.pruneUnreachable(start: String): Map<String, List<String>> {
+    var open = setOf(start)
+    val visited = mutableSetOf(start)
+
+    while (open.isNotEmpty()) {
+        visited += open
+        open = open.flatMap { this[it].orEmpty() }.toSet()
+    }
+
+    return this
+        .filterKeys { it in visited }
+        .mapValues { it.value.filter { it in visited } }
+}
+
+
+private fun Map<String, List<String>>.invert() = entries
+    .flatMap { (node, links) -> links.map { it to node } }
+    .groupBy({ it.first }, { it.second })
--- y2025/src/test/kotlin/adventofcode/y2025/Day11Test.kt
+++ y2025/src/test/kotlin/adventofcode/y2025/Day11Test.kt
@@ -7,13 +7,38 @@ internal class Day11Test {
 
     @Test
     fun solvePartOne() {
-        assertEquals(TODO(), Day11.solvePartOne(input))
+        assertEquals(5, Day11.solvePartOne(input))
     }
 
     @Test
     fun solvePartTwo() {
-        assertEquals(TODO(), Day11.solvePartTwo(input))
+        assertEquals(2, Day11.solvePartTwo(input2))
     }
 
-    private val input = """"""
+    private val input = """aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out"""
+
+
+    private val input2 = """svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out"""
+
 }
